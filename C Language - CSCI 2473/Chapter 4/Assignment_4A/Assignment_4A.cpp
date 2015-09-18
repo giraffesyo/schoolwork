1b7b7b20ed2f618c5cf180db6a985087bcc738bf
Michael McQuade <giraffesyo@gmail.com>
2015-09-18 04:18:41 -0500

    Assignment 4A
    Completed 4A assignment.

--- C Language - CSCI 2473/Chapter 4/Assignment_4A/Assignment_4A.cpp
+++ C Language - CSCI 2473/Chapter 4/Assignment_4A/Assignment_4A.cpp
@@ -2,4 +2,40 @@
 //C Programming
 //Chapter 4 Program A
 //Assignment_4A.cpp
-//TODO
+//This program calculates the roots of a quadratic equation
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double A, B, C;
+double X1, X2;
+
+void quadratic(double A, double B, double C)
+{
+	X1 = (-B + sqrt(B * B - 4 * A * C)) / (2 * A);
+	X2 = (-B - sqrt(B * B - 4 * A * C)) / (2 * A);
+}
+
+int main()
+{
+	cout << "This program calculates the two roots of a quadratic equation given A, B, and C." << endl;
+	cout << "Please enter A, B, and C: ";
+	
+	cin >> A >> B >> C;
+	
+	cout
+		<< endl
+		<< "A: " << A << endl
+		<< "B: " << B << endl 
+		<< "C: " << C << endl;
+
+	quadratic(A, B, C);
+	
+	cout 
+		<< "X1: " << X1 << endl
+		<< "X2: " << X2 << endl;
+
+	return 0;
+}
