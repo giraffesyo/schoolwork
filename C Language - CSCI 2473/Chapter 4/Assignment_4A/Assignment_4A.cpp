e6b61da7da615eb3f1af72c61cfe507bea8d2a76
Michael McQuade <giraffesyo@gmail.com>
2015-09-19 02:37:30 -0500

    Localized variables
    Localized variables by using references

--- C Language - CSCI 2473/Chapter 4/Assignment_4A/Assignment_4A.cpp
+++ C Language - CSCI 2473/Chapter 4/Assignment_4A/Assignment_4A.cpp
@@ -9,11 +9,8 @@
 
 using namespace std;
 
-double A, B, C;
-double X1, X2;
-
-//Quadratic Formula, calculates roots given floats A, B, C
-void quadratic()
+//Quadratic Formula, calculates roots given floats A, B, C, stores values in X1 and X2
+void quadratic(double A, double B, double C, double& X1, double& X2)
 {
 	X1 = (-B + sqrt(B * B - 4 * A * C)) / (2 * A);
 	X2 = (-B - sqrt(B * B - 4 * A * C)) / (2 * A);
@@ -21,9 +18,13 @@ void quadratic()
 
 int main()
 {
+	double X1, X2;
+	double A, B, C;
+
 	cout
-		<< "This program calculates the two roots of a quadratic equation given A, B, and C." << endl
-		<< "Please enter A, B, and C: ";
+		<< "This program calculates the two roots of a quadratic equation given A, B, and C." << endl << endl
+		<< "Press enter or space after each value." <<endl
+		<< "Please enter values for A, B, and C: ";
 	
 	cin >> A >> B >> C;
 	
@@ -33,7 +34,7 @@ int main()
 		<< "B: " << B << endl 
 		<< "C: " << C << endl;
  
-	quadratic();
+	quadratic(A, B, C, X1, X2);
 	
 	cout 
 		<< "X1: " << X1 << endl
