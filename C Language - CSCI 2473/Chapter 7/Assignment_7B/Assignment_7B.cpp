32cb1478d832896852c886a5e2eb86cd8cf6c4a8
Michael McQuade <giraffesyo@gmail.com>
2015-10-14 01:46:45 -0500

    Completed 7B
    Assignment 7B

--- C Language - CSCI 2473/Chapter 7/Assignment_7B/Assignment_7B.cpp
+++ C Language - CSCI 2473/Chapter 7/Assignment_7B/Assignment_7B.cpp
@@ -4,4 +4,30 @@
 //Assignment_7B.cpp
 //This program draws a figure using ASCII characters
 
-//todo
+#include <iostream>
+
+int main()
+{
+	for (int line = 1; line <= 7; line++)
+	{
+		for (int lessthan = 1; lessthan <= 7 - line; lessthan++)
+			std::cout << '<';
+		for (int space = 1; space <= line; space++)
+			std::cout << ' ';
+		for (int pound = 1; pound <= 14 - 2 * line; pound++)
+			std::cout << '#';
+		for (int asterisk = 1; asterisk <= line * 2 - 2; asterisk++)
+			std::cout << '*';
+		for (int space = 1; space <= line; space++)
+			std::cout << ' ';
+		for (int greaterthan = 1; greaterthan <= 7 - line; greaterthan++)
+			std::cout << '>';
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
+
+/*
+t
+*/
