4aebd1183452f14a5b50d2913506de47fa913f2e
Michael McQuade <giraffesyo@gmail.com>
2015-10-13 22:24:59 -0500

    Completed 6A
    Completed assignment 6A to guidelines, might come back in later and
    clean this up a bit with a function for each task but maybe not, two
    tests this week! ;)

--- C Language - CSCI 2473/Chapter 6/Assignment_6A/Assignment_6A.cpp
+++ C Language - CSCI 2473/Chapter 6/Assignment_6A/Assignment_6A.cpp
@@ -4,4 +4,176 @@
 //Assignment_6A.cpp
 //This program reads numbers from a data file, performs various functions and outputs to an answers file.
 
-//todo
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	ifstream dataFile;
+	ofstream answers;
+
+	int num;
+	int counter = 0;
+	double dataSum = 0.0;
+
+	dataFile.open("DataFile2.txt");
+	answers.open("answers.txt");
+		
+	//Read file and write all integers to one line
+	answers << "The input file contains the following data:" << endl;
+	dataFile >> num;
+	while(dataFile)
+	{	
+		answers << num << ' ';
+		dataFile >> num;
+	}
+
+	dataFile.close();
+	dataFile.clear();
+	dataFile.open("DataFile2.txt");
+
+	//Reread file and compute average of all integers in the file. 
+	answers << endl << endl << "The average of all numbers in the input file (with 3 decimal positions) is: ";
+	answers << fixed << setprecision(3);
+	
+	dataFile >> num;
+	while(dataFile)
+	{
+		dataSum += num;
+		counter++;
+		dataFile >> num;
+	}
+
+	answers << double(dataSum) / double(counter);
+
+
+	dataFile.close();
+	dataFile.clear();
+	dataFile.open("DataFile2.txt");
+
+
+	//Reread file and compute average of first 12 integers in the file. 
+	dataSum = 0;
+	counter = 1;
+
+	dataFile >> num;
+	while (counter <= 12)
+	{
+		dataSum += num;
+		counter++;
+		dataFile >> num;
+	}
+
+	answers << endl << endl << "The average of the first 12 numbers in the input file (with 3 decimal positions) is: ";
+	answers << double(dataSum) / double(counter - 1);
+
+	dataFile.close();
+	dataFile.clear();
+	dataFile.open("DataFile2.txt");
+	
+	//Reread file and get sum for every 3rd integer
+	dataSum = 0;
+	counter = 1;
+	dataFile >> num;
+	while (dataFile)
+	{
+		if (counter % 3 == 0) //then this is an iteration we want to capture!
+		{
+			dataSum += num;
+		}
+		counter++;
+		dataFile >> num;
+	}
+	answers << endl << endl << "The sum of every third integer in the file is: ";
+	answers << dataSum;
+
+
+	dataFile.close();
+	dataFile.clear();
+	dataFile.open("DataFile2.txt");
+
+
+	//Reread the file and determine the range of the numbers.
+	int smallest = INT_MAX;
+	int largest = INT_MIN;
+	
+	dataFile >> num;
+	while (dataFile)
+	{
+		if (largest < num)
+			largest = num;
+		if (smallest > num)
+			smallest = num;
+		dataFile >> num;
+	}
+	
+	answers << endl << endl << "The smallest number is " << smallest << " and the largest is " << largest << ".";
+
+
+	dataFile.close();
+	dataFile.clear();
+	dataFile.open("DataFile2.txt");
+
+	//Reread the file and determine the integer which is closest to 200.
+	int closest;
+	int diff;
+
+	dataFile >> num;
+	closest = num;
+	while (dataFile)
+	{
+		diff = abs(num - 200);
+		if ( diff < abs(closest - 200))
+			closest = num;
+		dataFile >> num;
+	}
+
+
+	answers << endl << endl << "The closest number to 200 is: " << closest;
+
+	dataFile.close();
+	dataFile.clear();
+	dataFile.open("DataFile2.txt");
+
+	//Reread the file and compute the average of all integers on the first two lines.
+	dataSum = 0;
+	counter = 0;
+	int lineCounter = 1;
+
+	dataFile >> num;
+	while (lineCounter <= 2)
+	{
+		if (dataFile.peek() == '\n')
+			lineCounter++;
+		dataSum += num;
+		counter++;
+		dataFile >> num;
+	}
+
+	answers << endl << endl << "The average of all integers on the first two lines is: ";
+	answers << double(dataSum) / double(counter);
+
+	dataFile.close();
+	dataFile.clear();
+	dataFile.open("DataFile2.txt");
+
+	//Reread the file and compute the sum of integers as long as the sum does not exceed 1000.
+	bool lessthan1000 = true;
+	dataSum = 0;
+
+	dataFile >> num;
+	while (lessthan1000)
+	{
+		if (dataSum + num > 1000)
+			lessthan1000 = false;
+		else
+			dataSum += num;
+	}
+
+	answers << endl << endl << "The sum of the integers without exceeding 1000 is: " << dataSum;
+
+
+	return 0;
+}
