5beb2972cad5a1396a7698d4e5120251bb3ca69a
Michael McQuade <giraffesyo@gmail.com>
2015-10-19 19:40:23 -0500

    Added function prototypes
    Added function prototypes and moved function definitions to below main.

--- C Language - CSCI 2473/Chapter 6/Assignment_6A/Assignment_6A.cpp
+++ C Language - CSCI 2473/Chapter 6/Assignment_6A/Assignment_6A.cpp
@@ -9,8 +9,69 @@
 
 using namespace std;
 
+//Function Prototypes
+void reopenData(ifstream& input);
+void allIntegers(ifstream& inputFile, ofstream& outputFile);
+double avgOfAll(ifstream& inputFile);
+double avgOf12(ifstream& inputFile);
+int sumevery3rd(ifstream& inputFile);
+void calculateRange(ifstream& inputFile, int& smallest, int& largest);
+int closest200(ifstream& inputFile);
+double avg2lines(ifstream& inputFile);
+int sumlessthan1000(ifstream& inputFile);
+
+//File Declarations
 const char inputFile[] = "DataFile2.txt";
 const char outputFile[] = "answers.txt";
+
+
+int main()
+{
+	ifstream dataFile;
+	ofstream answers;
+
+	dataFile.open(inputFile);
+	answers.open(outputFile);
+
+	//Task A
+	answers << "The input file contains the following data:" << endl;
+	allIntegers(dataFile, answers);
+
+	//Task B
+	answers << fixed << setprecision(3);
+	answers << endl << endl << "The average of all numbers in the input file (with 3 decimal positions) is: ";
+	answers << avgOfAll(dataFile);
+	
+	//Task C	
+	answers << endl << endl << "The average of the first 12 numbers in the input file (with 3 decimal positions) is: ";
+	answers << avgOf12(dataFile);
+
+	//Task D
+	answers << endl << endl << "The sum of every third integer in the file is: ";
+	answers << sumevery3rd(dataFile);
+
+	//Task E
+	int smallest = INT_MAX;
+	int largest = INT_MIN;
+	calculateRange(dataFile, smallest, largest);
+
+	answers << endl << endl << "The smallest number is: " << smallest;
+	answers << endl << "The largest number is: " << largest;
+	
+	//Task F
+	answers << endl << endl << "The closest number to 200 is: ";
+	answers << closest200(dataFile);
+
+	//Task G
+	answers << endl << endl << "The average of all integers on the first two lines is (with 3 decimal positions): ";
+	answers << avg2lines(dataFile);
+
+	//Task H
+	answers << endl << endl << "The sum of the integers without exceeding 1000 is: ";
+	answers << sumlessthan1000(dataFile) << endl;
+
+	return 0;
+}
 
 //Closes file stream, clears buffer, reopens stream.
 void reopenData(ifstream& input)
@@ -21,7 +82,7 @@ void reopenData(ifstream& input)
 }
 
 //Task A: Read file and write all integers to one line
-void allIntegers(ifstream& inputFile, ofstream& outputFile) 
+void allIntegers(ifstream& inputFile, ofstream& outputFile)
 {
 	int num;
 	inputFile >> num;
@@ -80,7 +141,7 @@ int sumevery3rd(ifstream& inputFile)
 	int dataSum = 0;
 	int counter = 1;
 	int num;
-	
+
 	inputFile >> num;
 	while (inputFile)
 	{
@@ -91,7 +152,7 @@ int sumevery3rd(ifstream& inputFile)
 		counter++;
 		inputFile >> num;
 	}
-	
+
 	reopenData(inputFile);
 	return dataSum;
 
@@ -101,7 +162,7 @@ int sumevery3rd(ifstream& inputFile)
 void calculateRange(ifstream& inputFile, int& smallest, int& largest)
 {
 	int num;
-	
+
 	inputFile >> num;
 	while (inputFile)
 	{
@@ -176,52 +237,4 @@ int sumlessthan1000(ifstream& inputFile)
 	}
 
 	return dataSum;
-}
-
-int main()
-{
-	ifstream dataFile;
-	ofstream answers;
-
-	dataFile.open(inputFile);
-	answers.open(outputFile);
-
-	//Task A
-	answers << "The input file contains the following data:" << endl;
-	allIntegers(dataFile, answers);
-
-	//Task B
-	answers << fixed << setprecision(3);
-	answers << endl << endl << "The average of all numbers in the input file (with 3 decimal positions) is: ";
-	answers << avgOfAll(dataFile);
-	
-	//Task C	
-	answers << endl << endl << "The average of the first 12 numbers in the input file (with 3 decimal positions) is: ";
-	answers << avgOf12(dataFile);
-
-	//Task D
-	answers << endl << endl << "The sum of every third integer in the file is: ";
-	answers << sumevery3rd(dataFile);
-
-	//Task E
-	int smallest = INT_MAX;
-	int largest = INT_MIN;
-	calculateRange(dataFile, smallest, largest);
-
-	answers << endl << endl << "The smallest number is: " << smallest;
-	answers << endl << "The largest number is: " << largest;
-	
-	//Task F
-	answers << endl << endl << "The closest number to 200 is: ";
-	answers << closest200(dataFile);
-
-	//Task G
-	answers << endl << endl << "The average of all integers on the first two lines is (with 3 decimal positions): ";
-	answers << avg2lines(dataFile);
-
-	//Task H
-	answers << endl << endl << "The sum of the integers without exceeding 1000 is: ";
-	answers << sumlessthan1000(dataFile) << endl;
-
-	return 0;
 }
