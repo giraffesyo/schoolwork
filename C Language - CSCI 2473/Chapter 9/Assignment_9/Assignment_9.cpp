8a9946aa5d56a9fb0180b4d02b2e954b1e719b43
Michael McQuade <giraffesyo@gmail.com>
2015-11-02 19:28:26 -0600

    Added comments
    Added comments and adjusted file output to make output not start with a
    blank line.

--- C Language - CSCI 2473/Chapter 9/Assignment_9/Assignment_9.cpp
+++ C Language - CSCI 2473/Chapter 9/Assignment_9/Assignment_9.cpp
@@ -1,3 +1,9 @@
+//Michael McQuade
+//C Programming
+//Chapter 9 Program
+//Assignment_9.cpp
+//This program performs various mathematical equations on geometrical shapes
+
 #include <iostream>
 #include <fstream>
 
@@ -26,84 +32,86 @@ int main()
 		selectedoption = Menu();
 		switch (selectedoption)
 		{
-			case 1 :  
-			{
-				double side;
-				cout << "Enter the length of a side: ";
-				cin >> side;
-				cout << endl << "The area of that square is: " << AreaOfSquare(side);
-				papertrail << endl << "The area of that square is: " << AreaOfSquare(side);
-				break;
-			}
-			case 2 :
-			{
-				double length, width;
-				cout << "Enter the length and width: ";
-				cin >> length >> width;
-				cout << endl << "The area of that rectangle is: " << AreaOfRectangle(length, width);
-				papertrail << endl << "The area of that rectangle is: " << AreaOfRectangle(length, width);
-				break;
-			}
-			case 3 : 
-			{
-				double side1, side2;
-				cout << "Enter length of the two legs: ";
-				cin >> side1 >> side2;
-				cout << endl << "The hypotenuse of that right triangle is: " << HypotenuseOfRTri(side1, side2);
-				papertrail << endl << "The hypotenuse of that right triangle is: " << HypotenuseOfRTri(side1, side2);
-				break;
-			}
-			case 4 :
-			{
-				double side1, side2;
-				cout << "Enter length of the two legs: ";
-				cin >> side1 >> side2;
-				cout << endl << "The area of that right triangle is: " << AreaOfRTri(side1, side2);
-				papertrail << endl << "The area of that right triangle is: " << AreaOfRTri(side1, side2);
-				break;
-			}
-			case 5 :
-			{
-				double height, base;
-				cout << "Enter height and base: ";
-				cin >> height >> base;
-				cout << endl << "The area of that isosceles triangle is: " << AreaOfIsoTri(height, base);
-				papertrail << endl << "The area of that isosceles triangle is: " << AreaOfIsoTri(height, base);
-				break;
-			}
-			case 6 : 
-			{
-				double radius;
-				cout << "Enter radius: ";
-				cin >> radius;
-				cout << endl << "The circumference of that circle is: " << CircumferenceOfCircle(radius);
-				papertrail << endl << "The circumference of that circle is: " << CircumferenceOfCircle(radius);
-				break;
-			}
-			case 7 : 
-			{
-				double radius;
-				cout << "Enter radius: ";
-				cin >> radius;
-				cout << endl << "The area of that circle is: " << AreaOfCircle(radius);
-				papertrail << endl << "The area of that circle is: " << AreaOfCircle(radius);
-				break;
-			}
-			case 8 : 
-			{
-			double radius;
-			cout << "Enter radius: ";
-			cin >> radius;
-			cout << "The volume of that sphere is: " << VolumeOfSphere(radius);
-			papertrail << "The volume of that sphere is: " << VolumeOfSphere(radius);
-			break;
-			}
-			case 99 : return 0;
+		case 1:
+		{
+				  double side;
+				  cout << "Enter the length of a side: ";
+				  cin >> side;
+				  cout << endl << "The area of that square is: " << AreaOfSquare(side);
+				  papertrail << "The area of that square is: " << AreaOfSquare(side);
+				  break;
+		}
+		case 2:
+		{
+				  double length, width;
+				  cout << "Enter the length and width: ";
+				  cin >> length >> width;
+				  cout << endl << "The area of that rectangle is: " << AreaOfRectangle(length, width);
+				  papertrail << "The area of that rectangle is: " << AreaOfRectangle(length, width);
+				  break;
+		}
+		case 3:
+		{
+				  double side1, side2;
+				  cout << "Enter length of the two legs: ";
+				  cin >> side1 >> side2;
+				  cout << endl << "The hypotenuse of that right triangle is: " << HypotenuseOfRTri(side1, side2);
+				  papertrail << "The hypotenuse of that right triangle is: " << HypotenuseOfRTri(side1, side2);
+				  break;
+		}
+		case 4:
+		{
+				  double side1, side2;
+				  cout << "Enter length of the two legs: ";
+				  cin >> side1 >> side2;
+				  cout << endl << "The area of that right triangle is: " << AreaOfRTri(side1, side2);
+				  papertrail << "The area of that right triangle is: " << AreaOfRTri(side1, side2);
+				  break;
+		}
+		case 5:
+		{
+				  double height, base;
+				  cout << "Enter height and base: ";
+				  cin >> height >> base;
+				  cout << endl << "The area of that isosceles triangle is: " << AreaOfIsoTri(height, base);
+				  papertrail << "The area of that isosceles triangle is: " << AreaOfIsoTri(height, base);
+				  break;
+		}
+		case 6:
+		{
+				  double radius;
+				  cout << "Enter radius: ";
+				  cin >> radius;
+				  cout << endl << "The circumference of that circle is: " << CircumferenceOfCircle(radius);
+				  papertrail << "The circumference of that circle is: " << CircumferenceOfCircle(radius);
+				  break;
+		}
+		case 7:
+		{
+				  double radius;
+				  cout << "Enter radius: ";
+				  cin >> radius;
+				  cout << endl << "The area of that circle is: " << AreaOfCircle(radius);
+				  papertrail << "The area of that circle is: " << AreaOfCircle(radius);
+				  break;
+		}
+		case 8:
+		{
+				  double radius;
+				  cout << "Enter radius: ";
+				  cin >> radius;
+				  cout << endl << "The volume of that sphere is: " << VolumeOfSphere(radius);
+				  papertrail << "The volume of that sphere is: " << VolumeOfSphere(radius);
+				  break;
+		}
+		case 99: return 0;
 		}
 		cout << endl << endl;
+		papertrail << endl;
 	}
 }
 
+//Prompts for a choice to be made to determine program operation
 int Menu()
 {
 	cout << "Select one of the following calculations:" << endl;
@@ -123,41 +131,49 @@ int Menu()
 	return choice;
 }
 
+//Returns the area of a square given the length of a side
 double AreaOfSquare(double side)
 {
 	return side * side;
 }
 
+//Returns the area of a rectangle given the length and width
 double AreaOfRectangle(double length, double width)
 {
 	return length * width;
 }
 
+//Returns the Hypotenuse of a right triangle given the length of the two legs
 double HypotenuseOfRTri(double side1, double side2)
 {
 	return sqrt(side1 * side1 + side2 * side2);
 }
 
+//Returns the area of a right triangle given the length of the two legs
 double AreaOfRTri(double side1, double side2)
 {
 	return side1 * side2 / 2;
 }
 
+//Returns the Area of isosocles triangle given the height and the base
 double AreaOfIsoTri(double height, double base)
 {
 	return height * base / 2;
 }
 
+//Returns the circumference of a circle given its radius
 double CircumferenceOfCircle(double radius)
 {
 	return 2 * radius * Pi;
 }
 
+//Returns the area of a circle given its radius
 double AreaOfCircle(double radius)
 {
 	return radius * radius * Pi;
 }
 
+//Returns the volume of a sphere given its radius
 double VolumeOfSphere(double radius)
 {
 	return 4 * Pi * radius * radius * radius / 3;
