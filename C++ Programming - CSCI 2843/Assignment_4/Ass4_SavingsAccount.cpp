63d3281b1e88819ff747495b53fda4fbf66de907
Michael McQuade <giraffesyo@gmail.com>
2016-04-10 21:08:44 -0500

    Added documentation

--- C++ Programming - CSCI 2843/Assignment_4/Ass4_SavingsAccount.cpp
+++ C++ Programming - CSCI 2843/Assignment_4/Ass4_SavingsAccount.cpp
@@ -10,9 +10,10 @@
 #include "Ass4_SavingsAccount.h"
 
 
-int SavingsAccount::objectCount = 0;
-double SavingsAccount::annualInterestRate = 0.0;
+int SavingsAccount::objectCount = 0; // This number will be one behind objectNumber at all times.
+double SavingsAccount::annualInterestRate = 0.0; //Shared by all accounts
 
+//Default constructor which initializes data members to empty strings and 0 values, object assigned objectCount+1.
 SavingsAccount::SavingsAccount() : objectNumber(objectCount + 1)
 {
 	firstName = "";
@@ -21,6 +22,7 @@ SavingsAccount::SavingsAccount() : objectNumber(objectCount + 1)
 	objectCount++;
 }
 
+//Consturctor with only name given, initializes balance to 0.0; object assigned objectCount+1
 SavingsAccount::SavingsAccount(std::string name) : objectNumber(objectCount + 1)
 {
 	parseName(name);
@@ -28,6 +30,7 @@ SavingsAccount::SavingsAccount(std::string name) : objectNumber(objectCount + 1)
 	objectCount++;
 }
 
+//Full constructor, initializes name to passed value and balance to passed value. object assigned objectCount+1
 SavingsAccount::SavingsAccount(std::string name, double startBalance) : objectNumber(objectCount + 1)
 {
 	parseName(name);
@@ -35,39 +38,55 @@ SavingsAccount::SavingsAccount(std::string name, double startBalance) : objectNu
 	objectCount++;
 }
 
+//Destructor - when an account object goes out of scope will print a message to the screen.
 SavingsAccount::~SavingsAccount()
 {
 	std::cout << "Object has gone out of scope" << std::endl;
 }
 
+//setName is alias for parseName function which handles string types.
 void SavingsAccount::setName(std::string name)
 {
 	parseName(name);
 }
+
+//Concatenates firstName and lastName members and returns them to caller
 std::string SavingsAccount::getName() const
 {
 	return firstName + " " + lastName;
 }
+
+//Sets the account balance to passed double.
 void SavingsAccount::setBalance(double newBalance)
 {
 	savingBalance = newBalance;
 }
+
+//Returns account balance.
 double SavingsAccount::getBalance() const
 {
 	return savingBalance;
 }
+
+//Sets the interest rate. Pass the interest rate naturally e.g setInterestRate(4.50)
 void SavingsAccount::setInterestRate(double newInterestRate)
 {
 	annualInterestRate = newInterestRate / 100.0;
 }
+
+//Returns the interest rate for all accounts. Returns it naturally e.g 4.5
 double SavingsAccount::getInterestRate() 
 {
 	return annualInterestRate * 100.0;
 }
+
+//Returns the account number for the object.
 double SavingsAccount::getNumber() const
 {
 	return objectNumber;
 }
+
+//Outputs to the screen all information about the account.
 void SavingsAccount::display() const
 {
 	std::cout << std::fixed << std::showpoint << std::setprecision(2);
@@ -76,11 +95,15 @@ void SavingsAccount::display() const
 	std::cout << std::setprecision(0) << std::noshowpoint;
 	std::cout << "Account Number: " << getNumber() << std::endl << std::endl;
 }
+
+//Calculates the new balance of an account after 1 month of interest has accrued.
 void SavingsAccount::calculateNewBalance()
 {
 	savingBalance += annualInterestRate / 12.0 * savingBalance;
 }
 
+//ParseName function used privately within class
+//reads the received name string and sets account firstName to anything before first space, everything after is lastName. If no last name, only sets the first name.
 void SavingsAccount::parseName(std::string name)
 {
 	if (name.find(" ") != std::string::npos)
@@ -92,17 +115,19 @@ void SavingsAccount::parseName(std::string name)
 		firstName = name;
 }
 
+//Allows increase of account balance with operator+=
 void SavingsAccount::operator+=(const double ammountToIncrease)
 {
 	setBalance(getBalance() + ammountToIncrease);
 }
 
+//Allows decrease of account balance with operator-=
 void SavingsAccount::operator-=(const double ammountToDecrease)
 {
 	setBalance(getBalance() - ammountToDecrease);
 }
 
-
+//Requests input for keyboard which will populate a SavingsAccount object
 std::istream & operator>>(std::istream & input, SavingsAccount & account)
 {
 	std::cout << "Please enter in an an account name: ";
@@ -117,6 +142,7 @@ std::istream & operator>>(std::istream & input, SavingsAccount & account)
 	return input;
 }
 
+//displays information on the screen about received SavingsAccount 
 std::ostream & operator<<(std::ostream & output, const SavingsAccount & account)
 {
 	account.display();
