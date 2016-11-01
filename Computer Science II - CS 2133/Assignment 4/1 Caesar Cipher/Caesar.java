9e75103e5d7914dab1f3f4da8b056177de5aacf0
Michael McQuade <michael.mcquade@okstate.edu>
2016-11-01 14:07:18 -0500

    Completed Caesar
    I uploaded the wrong file originally, this is the correct Caesar.java
    file

--- Computer Science II - CS 2133/Assignment 4/1 Caesar Cipher/Caesar.java
+++ Computer Science II - CS 2133/Assignment 4/1 Caesar Cipher/Caesar.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * Created by mmcquad on 10/6/16.
  */
@@ -5,30 +7,83 @@ public class Caesar {
     public static void main(String[] args)
     {
 		//Close and print help if incorrect arguments
-        if (args.length != 3) {
+        if (args.length < 2 || args.length > 3) {
             usageHelp();
             return;
         }
-		
+
+        //Determine File or Screen output
+        boolean OutputToFile = false;
+        if (args.length == 3)
+        {
+            OutputToFile = true;
+        }
+
         //Argument constants
         final int key = Integer.parseInt(args[0]);
 		final String inFileName = args[1];
-		final String outFileName = args[2];
-
-		
-		InputStream inFile = new InputStream(inFileName);
-		OutputStream outFile = new OutputStream(outFileName);
-		
-		while(InputStream)
+        final String outFileName;
+		if (OutputToFile)
 		{
-			
-		}
-		
-		
-		
-		
-        System.out.println(caesarShift('K',key));
+            outFileName = args[2];
+        }
+        else
+        {
+            outFileName = null;
+        }
 
+        //input setup
+        BufferedReader input;
+        try{
+            input = new BufferedReader(new FileReader(inFileName));
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            return; //so we don't hit while loop without intialized input
+        }
+
+        //output setup
+        BufferedWriter output;
+        if (OutputToFile) {
+            try
+            {
+                output = new BufferedWriter(new FileWriter(outFileName));
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+                return; // so we don't hit while loop without initialized output
+            }
+        }
+        else
+        {
+            output = null;
+        }
+
+
+        //Main processing of data
+        while(true)
+		{
+		    try
+            {
+                int next = input.read();
+                if(next == -1)
+                    break;
+                if (OutputToFile) {
+                    output.write(caesarShift((char)next, key));
+                    output.flush();
+                } else {
+                    System.out.print(caesarShift((char)next,key));
+                }
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+                break;
+            }
+
+        }
 
     }
 
@@ -46,6 +101,6 @@ public class Caesar {
 
     public static void usageHelp()
     {
-        System.out.println("Invalid number of arguments. Usage: java Caesar key infile outfile");
+        System.out.println("Invalid number of arguments. Usage: java Caesar key infile [outfile]");
     }
 }
