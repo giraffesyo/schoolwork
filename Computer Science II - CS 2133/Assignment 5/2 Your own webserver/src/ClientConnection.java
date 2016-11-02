f918c752c5a377f26d0f4dd0ce83d3ce03a0e20b
Michael McQuade <mmcquad@mac01-cs.ms-2345.okstate.edu>
2016-11-02 03:17:09 -0500

    Webserver final
    Completed web server portion of assignment 5

--- Computer Science II - CS 2133/Assignment 5/2 Your own webserver/src/ClientConnection.java
+++ Computer Science II - CS 2133/Assignment 5/2 Your own webserver/src/ClientConnection.java
@@ -1,17 +1,28 @@
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 
 public class ClientConnection implements Runnable {
 
     private Socket s;
-    String request;
-    BufferedReader file;
-    final boolean debug = true;
 
-    String OKString = "HTTP/1.1 200 OK\r\n";
-    String ContentTypeHTML = "Content-type: text/html\r\n\r\n";
-    String NotFoundString = "HTTP/1.1 404 Not Found\r\n\r\n";
-    String InternalErrorString = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+    BufferedReader bufferedReader;
+    PrintWriter printWriter;
+    int contentType = 0;
+
+
+    final String OKString = "HTTP/1.1 200 OK\r\n";
+    final String NotFoundString = "HTTP/1.1 404 Not Found\r\n\r\n";
+    final String InternalErrorString = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+
+    final String ContentTypeHTML = "Content-type: text/html\r\n\r\n";
+    final String ContentTypeJPG = "Content-type: image/jpeg\r\n\r\n";
+    final String ContentTypePNG = "Content-type: image/png\r\n\r\n";
+    final String ContentTypeICO = "Content-type: image/x-icon\r\n\r\n";
+
 
     ClientConnection(Socket socket) {
         s = socket;
@@ -20,121 +31,100 @@ public class ClientConnection implements Runnable {
 
     public void run() {
         try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            OutputStreamWriter out = new OutputStreamWriter(s.getOutputStream(), "UTF-8");
-            out.write(ContentTypeHTML, 0, ContentTypeHTML.length());
-            out.flush();
-            String line;
-            request = "";
-            while ((line = in.readLine()) != null) {
-                request += line + "\r\n";
-            }
-            request += "\r\n";
-            switch (processRequest()) {
-                case 0:
-                    if(debug)
-                        System.out.println("Reached sendSuccess()");
+
+            //setup and receive socket connection
+            bufferedReader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            OutputStream out = s.getOutputStream();
+            printWriter = new PrintWriter(out, true);
 
 
+            //Gets file name from input socket
+            String fileName = getRequestedFileString();
+            System.out.println("The client has requested: " + fileName);
 
-                    out.write(OKString);
-                    out.flush();
-
-                    if (debug)
-                        System.out.println(OKString);
-
-
-
-                        while ((line = file.readLine()) != null) {
-                            line += "\r\n";
-                            out.write(line, 0, line.length());
-                            out.flush();
-                            if (debug)
-                                System.out.println(line + "\r\n");
-                        }
-
-                    out.flush();
-                    break;
-                case 2:
-                    sendFileNotFound(out);
-                    break;
+            //ContentType
+            if (new File(fileName).exists()) {
+                printWriter.print(OKString);
+                printWriter.flush();
+                System.out.println("Sent client message 200, connection OK");
+                if (contentType == 0) {
+                    printWriter.print(ContentTypeHTML);
+                    printWriter.flush();
+                    System.out.println("Sent client HTML Content Type");
+                } else if (contentType == 1) {
+                    printWriter.print(ContentTypeJPG);
+                    printWriter.flush();
+                    System.out.println("Sent client JPG Content Type");
+                } else if (contentType == 2) {
+                    printWriter.print(ContentTypePNG);
+                    printWriter.flush();
+                    System.out.println("Sent client PNG Content Type");
+                } else if (contentType == 3) {
+                    printWriter.print(ContentTypeICO);
+                    printWriter.flush();
+                    System.out.println("Sent client ICO Content Type");
+                }
             }
 
+
+            //Reads the file that we got from getRequestedFileString() and sends it through our socket
+            Path filePath = FileSystems.getDefault().getPath(fileName);
+            Files.copy(filePath, out);
+            printWriter.flush();
+            printWriter.close();
+
+        } catch (NoSuchFileException e) {
+            printWriter.print(NotFoundString);
+            printWriter.println("File not found");
+            System.out.println("Sent client 404 header");
+            printWriter.close();
         } catch (IOException e) {
+            printWriter.print(InternalErrorString);
+            printWriter.flush();
+            System.out.println("Sent client Internal Server Error header");
+            printWriter.close();
             e.printStackTrace();
         }
     }
 
-    //strips request and returns result
-    private int processRequest() {
-        int result;
-        if (debug)
-            System.out.println("request at top of processRequest() is: " + request);
-        if (request.contains("GET")) {
-            request = request.substring(request.indexOf("GET") + 5);
-            request = request.substring(0, request.indexOf(".html") + 5);
-            result = 0;
-            if (debug) {
-                System.out.println("result substring in processRequest() is: " + request);
-                System.out.println("int result in processRequest() is: " + result);
-            }
-        } else {
-            result = 1;
-            if (debug)
-                System.out.println("int result in processRequest() is: " + result);
-            return result;
-        }
+    String getRequestedFileString() {
+        String RequestedFile = "";
 
+        //catch directory manipulators
 
         try {
-            file = new BufferedReader(new FileReader(request));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return 2;
-        }
+            RequestedFile = bufferedReader.readLine();
 
-        return result;
-    }
-
- /*   private void sendSuccess(OutputStreamWriter out) throws IOException {
-        if(debug)
-            System.out.println("Reached sendSuccess()");
-
-
-
-        out.write(OKString);
-        out.flush();
-
-        if (debug)
-            System.out.println(OKString);
-
-        String line;
-        try {
-            while ((line = file.readLine()) != null) {
-                line += "\r\n";
-                out.write(line, 0, line.length());
-                out.flush();
-                if (debug)
-                    System.out.println(line + "\r\n");
+            //strip .. so no funny business from client
+            if(RequestedFile.contains("..")) {
+            RequestedFile = RequestedFile.replace("\\.\\.","");
             }
-        } catch (IOException e) {
+
+
+            //Removes everything besides file name
+            RequestedFile = RequestedFile.replaceAll("GET /*", "");
+            RequestedFile = RequestedFile.replaceAll(" HTTP[/s/S]*.*", "");
+
+            if (RequestedFile.contains(".jpg")) {
+                contentType = 1;
+            } else if (RequestedFile.contains(".png")) {
+                contentType = 2;
+            } else if (RequestedFile.contains(".ico")) {
+                contentType = 3;
+            }
+
+            //Give index.html if they don't choose a page on the server
+            if (RequestedFile.equals("")) {
+                RequestedFile = "index.html";
+            }
+        } catch (IOException e)
+
+        {
             e.printStackTrace();
         }
-        out.flush();
-    }
-*/
-    private void sendFileNotFound(OutputStreamWriter out) throws IOException {
 
-        out.write(NotFoundString);
-        out.flush();
-        if (debug)
-            System.out.println(NotFoundString);
+        return RequestedFile;
     }
 
-    private void sendInternalServerError(OutputStreamWriter out) throws IOException {
-
-        out.append(InternalErrorString);
-        if (debug)
-            System.out.println(InternalErrorString);
-    }
 }
--- Computer Science II - CS 2133/Assignment 5/2 Your own webserver/src/Webserver.java
+++ Computer Science II - CS 2133/Assignment 5/2 Your own webserver/src/Webserver.java
@@ -2,13 +2,14 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 
-public class Webserver {
 
+public class Webserver {
+    final static int port = 8080;
 
     public static void main(String[] args) {
         ServerSocket server;
         try {
-            server = new ServerSocket(8080);
+            server = new ServerSocket(port);
             while (true) {
                 Thread conn = new Thread(new ClientConnection(server.accept()));
                 conn.start();
