9349fbb9e3889ce6837d3b75a118bedd4cb674c8
Michael McQuade <michael.mcquade@okstate.edu>
2017-02-25 18:19:18 -0600

    Scrap Huffman
    Not working code

--- Computer Science II - CS 2133/Assignment 6/3 Huffman encoding and decoding/Huff.java
+++ Computer Science II - CS 2133/Assignment 6/3 Huffman encoding and decoding/Huff.java
@@ -1,23 +1,38 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+<<<<<<< Updated upstream
 import java.util.HashMap;
 
+=======
+import java.util.*;
+>>>>>>> Stashed changes
 
 public class Huff {
 
-    public static final int BytePerms = 256;
+    //public static final int BytePerms = 256; //not used
 
     public static FileInputStream input;
     FileOutputStream output;
     public static byte[] bytes;
 
 
-    public static void main(String[] args)
-    {
-        String FileName = args[0];
+    public static void main(String[] args) {
+        //ensure filename specified
+        /*if (args.length != 1)
+        {
+            System.out.println("Usage: java Huff fileName.ext");
+            System.exit(1);
+        }
+*/
+
+
+        //String FileName = args[0];
+        String FileName = "/assn1.pdf"; //temp
+
 
         try {
+<<<<<<< Updated upstream
             input = new FileInputStream(FileName);
             bytes = new byte[input.available()];
             input.read(bytes);
@@ -26,7 +41,79 @@ public class Huff {
 
         }catch(IOException e)
         {
+=======
+            input = new FileInputStream(FileName); //file to get bytes from
+            bytes = new byte[input.available()]; //available is total amount of bytes in file
+            input.read(bytes); // reads file in as bytes and stores into "bytes"
 
+            HashMap<Byte, Integer> frequencies = new HashMap<>();
+
+            //count frequencies by putting bytes into hashmap
+            for (byte b : bytes) {
+                if (frequencies.containsKey(b)) {
+                    frequencies.replace(b, frequencies.get(b) + 1);
+                } else {
+                    frequencies.put(b, 1);
+                }
+
+
+            }
+
+
+
+
+            /*
+            //create iterator with each keypaid, call fqiterator.next to get each pair
+            //values return such as "-1=685"
+            //Iterator fqiterator = frequencies.entrySet().iterator();
+
+            //print keypairs
+            while(fqiterator.hasNext())
+            {
+                System.out.println(fqiterator.next());
+            }
+            */
+
+            PriorityQueue<HuffmanNode> NodesPQ = new PriorityQueue<>();
+
+            for (int i = -128; i < 128; i++) {
+                //use Integer object so we can store null
+                Integer currentFreq = frequencies.get((byte) (i));
+
+                //null is returned by Map.get() when no key matched
+                if (currentFreq == null)
+                    continue; // skip to next iteration
+                NodesPQ.add(new HuffmanNode((byte) (i), currentFreq));
+
+
+            }
+
+            //Connect all the nodes
+            while (NodesPQ.size() > 1) {
+                NodesPQ.add(new HuffmanNode(NodesPQ.poll(), NodesPQ.poll()));
+            }
+
+            //Last node forms a tree!
+            HuffmanNode Tree = NodesPQ.poll();
+
+
+
+            //test tree and see where bottom is
+            HuffmanNode test = Tree.one;
+
+            while(test.zero != null)
+            {
+                test = test.one;
+                System.out.print(test.getFreq());
+                System.out.print(", ");
+                System.out.print(test.getValue());
+                System.out.println();
+            }
+
+>>>>>>> Stashed changes
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
