23ac0ef8ffd25354bdd88ff8630305ea085686d2
Michael McQuade <giraffesyo@gmail.com>
2017-03-27 20:40:51 -0500

    Use absolute value for bearing
    Not using PID for distance, however need to still tweak final constant values for bearing PID controller, this push fixes the search so we are using absolute value of bearing: before we would get negative results which would cause it to prematurely exit the search mode. Now the bot will properly turn left or right to adjust, but at incorrect speeds.

--- Programming Intelligent Robots - HONR 1000/Assignment 4/Assignment4.py
+++ Programming Intelligent Robots - HONR 1000/Assignment 4/Assignment4.py
@@ -23,20 +23,20 @@ class Robot:
         self.lastChange = 0
 
         self.bearingGoal = 320
-        self.bearingP = .02
-        self.bearingI = .02
-        self.bearingD = .02
+        self.bearingP = 1/320
+        self.bearingI = .5
+        self.bearingD = .005
 
         self.bearingPID = PID(self.bearingGoal, self.bearingP, self.bearingI, self.bearingD)
 
         self.distanceGoal = 1.5
-        self.distanceP = 1
-        self.distanceI = 1
-        self.distanceD = 1
+        self.distanceP = .5
+        self.distanceI = 0
+        self.distanceD = 0
         self.distancePID = PID(self.distanceGoal, self.distanceP, self.distanceI, self.distanceD)
 
-        self.ballBearing = -1
-        self.ballDistance = -1
+        self.ballBearing = -1.0
+        self.ballDistance = -1.0
 
     def ballLocation(self, msg):
         # Store ball location into instance variables so we can access them in state machine loop
@@ -50,47 +50,45 @@ class Robot:
             # print("We're in state: " + self.state) #spams state
             if self.state == "approach":  # "go forward"
                 # make sure we shouldn't be in search
-                if (self.ballDistance == -1) or (self.ballBearing == -1):
+                if (self.ballDistance == -1) or (self.ballBearing == -1) or ((abs(self.ballBearing - self.bearingGoal)) < 5):
                     self.state = "search"
                 else:
                     # need a way to stop using PID
                     # and starting moving
                     # Set twist.angular.z to a positive PID value
-                    if self.bearingGoal - self.ballBearing < 5:
-                        if self.ballDistance > 1.5:
-                            twist.angular.z = 0
-                            twist.linear.x = 1
-                            self.Twistpub.publish(twist)
-                        else:
-                            self.lastChange = rospy.Time.now()
-                            self.state = "kick"
-                    else:
-                        twist.linear.x = 0
-                        print "bearingPID.get results: " + str(self.bearingPID.get_output(self.ballBearing))
-                        twist.angular.z = self.bearingPID.get_output(self.ballBearing)*.01
-                        # Publish twist
+                    if self.ballDistance > 1.5:
+                        twist.angular.z = 0
+                        twist.linear.x = .2
                         self.Twistpub.publish(twist)
+                    else:
+                        self.lastChange = rospy.Time.now()
+                        self.state = "kick"
                     # when ready to implement kick we set timer here (self.lastchange)
+            elif self.state == "search":
+                # If we don't see the ball at all
+                if self.ballBearing == -1 or self.ballDistance == -1:
+                    twist.linear.x = 0
+                    twist.angular.z = .5
+                    self.Twistpub.publish(twist)
+                else:
+                    # If we see the ball and its within the right range
+                    if abs(self.bearingGoal - self.ballBearing) < 5:
+                        self.state = "approach"
+                    else: # we see the ball but we need to line up our bearings
+                        twist.linear.x = 0
+                        twist.angular.z = self.bearingPID.get_output(self.ballBearing)
+                        print "Angular z:", str(twist.angular.z)
+                        self.Twistpub.publish(twist)
             elif self.state == "kick":  # Drive forward at 1 m/s for 1.5 seconds
                 twist.linear.x = 1
                 twist.angular.z = 0
                 self.Twistpub.publish(twist)
                 # If enough time has elapsed, change to the "turn" state
                 if (rospy.Time.now() - self.lastChange) > rospy.Duration.from_sec(1.5):
-                    print("More than 1.5 seconds has elapsed, searching for ball")
+                    print "More than 1.5 seconds has elapsed, searching for ball"
                     self.state = "search"
                     self.lastChange = rospy.Time.now()
-            elif self.state == "search":  # "turn"  # Check to see if we have a valid location
-                print "self.ballDistance: " + str(self.ballDistance)
-                print "self.ballBearing: " + str(self.ballBearing)
-                if (self.ballDistance != -1) and (self.ballBearing != -1):
-                    self.state = "approach"
-                else:
-                    # Turning at 1 radian per second
-                    twist.linear.x = 0
-                    twist.angular.z = .5
-                    # Publish twist message
-                    self.Twistpub.publish(twist)
+            print "We're in state: " + self.state, " Distance is: ", self.ballDistance, " Bearing is", self.ballBearing 
             rate.sleep()
 
 
