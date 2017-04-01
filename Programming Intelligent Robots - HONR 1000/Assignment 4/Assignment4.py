2020d3a38d6d49bab2aca34aa3c398ef16a1e753
Michael McQuade <giraffesyo@gmail.com>
2017-04-01 17:30:18 -0500

    Finalized version
    Working robot puppy

--- Programming Intelligent Robots - HONR 1000/Assignment 4/Assignment4.py
+++ Programming Intelligent Robots - HONR 1000/Assignment 4/Assignment4.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import math
 from geometry_msgs.msg import Twist
 from assignment3.msg import BallLocation
 
@@ -21,16 +22,18 @@ class Robot:
         self.state = "search"
         # and a variable to keep track of the time of the last state change
         self.lastChange = 0
+        self.maxLinearX = 1.0
+        self.maxAngularZ = 1.0
 
         self.bearingGoal = 320
-        self.bearingP = 1/320
-        self.bearingI = .5
-        self.bearingD = .005
+        self.bearingP = .003125 #1/320
+        self.bearingI = .00001
+        self.bearingD = .25
 
         self.bearingPID = PID(self.bearingGoal, self.bearingP, self.bearingI, self.bearingD)
 
         self.distanceGoal = 1.5
-        self.distanceP = .5
+        self.distanceP = -0.4
         self.distanceI = 0
         self.distanceD = 0
         self.distancePID = PID(self.distanceGoal, self.distanceP, self.distanceI, self.distanceD)
@@ -50,16 +53,15 @@ class Robot:
             # print("We're in state: " + self.state) #spams state
             if self.state == "approach":  # "go forward"
                 # make sure we shouldn't be in search
-                if (self.ballDistance == -1) or (self.ballBearing == -1) or ((abs(self.ballBearing - self.bearingGoal)) < 5):
+                if (self.ballDistance == -1) or (self.ballBearing == -1):
                     self.state = "search"
                 else:
                     # need a way to stop using PID
                     # and starting moving
                     # Set twist.angular.z to a positive PID value
-                    if self.ballDistance > 1.5:
-                        twist.angular.z = 0
-                        twist.linear.x = .2
-                        self.Twistpub.publish(twist)
+                    if abs(self.ballDistance - 1.5) > .25 or ((abs(self.ballBearing - self.bearingGoal)) > 10):
+                        twist.angular.z = self.bearingPID.get_output(self.ballBearing)
+                        twist.linear.x = self.distancePID.get_output(self.ballDistance)
                     else:
                         self.lastChange = rospy.Time.now()
                         self.state = "kick"
@@ -68,27 +70,27 @@ class Robot:
                 # If we don't see the ball at all
                 if self.ballBearing == -1 or self.ballDistance == -1:
                     twist.linear.x = 0
-                    twist.angular.z = .5
-                    self.Twistpub.publish(twist)
+                    twist.angular.z = self.maxAngularZ
                 else:
-                    # If we see the ball and its within the right range
-                    if abs(self.bearingGoal - self.ballBearing) < 5:
-                        self.state = "approach"
-                    else: # we see the ball but we need to line up our bearings
-                        twist.linear.x = 0
-                        twist.angular.z = self.bearingPID.get_output(self.ballBearing)
-                        print "Angular z:", str(twist.angular.z)
-                        self.Twistpub.publish(twist)
+                    self.state = "approach"
             elif self.state == "kick":  # Drive forward at 1 m/s for 1.5 seconds
-                twist.linear.x = 1
+                twist.linear.x = self.maxLinearZ
                 twist.angular.z = 0
-                self.Twistpub.publish(twist)
                 # If enough time has elapsed, change to the "turn" state
                 if (rospy.Time.now() - self.lastChange) > rospy.Duration.from_sec(1.5):
                     print "More than 1.5 seconds has elapsed, searching for ball"
                     self.state = "search"
                     self.lastChange = rospy.Time.now()
-            print "We're in state: " + self.state, " Distance is: ", self.ballDistance, " Bearing is", self.ballBearing 
+            print "We're in state: " + self.state, " Distance is: ", self.ballDistance, " Bearing is", self.ballBearing
+            if twist.linear.x > self.maxLinearX:
+                twist.linear.x = self.maxLinearX
+            if twist.linear.x < -self.maxLinearX:
+                twist.linear.x = -self.maxLinearX
+            if twist.angular.z < -self.maxAngularZ:
+                twist.angular.z = -self.maxAngularZ
+            if twist.angular.z > self.maxAngularZ:
+                twist.angular.z = self.maxAngularZ            
+            self.Twistpub.publish(twist) 
             rate.sleep()
 
 
