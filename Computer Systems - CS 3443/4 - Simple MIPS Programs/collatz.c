542f998ff592f08cc2f6cfa22a182f98dedfc6e4
Michael McQuade <michael.mcquade@okstate.edu>
2017-05-04 22:00:19 -0500

    Collatz in C

---
+++ Computer Systems - CS 3443/4 - Simple MIPS Programs/collatz.c
@@ -0,0 +1,11 @@
+// Created by Michael McQuade on 5/1/17.
+#include <stdio.h>
+
+int main() {
+    //Print my name
+    printf("%s\n", "Michael McQuade");
+
+    printf("%s", "Please enter a number: ");
+
+    return 0;
+}
