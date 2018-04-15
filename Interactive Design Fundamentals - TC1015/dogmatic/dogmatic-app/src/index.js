9449a687f255de816b23b687611fb570b35b703c
Michael McQuade <giraffesyo@gmail.com>
2018-04-15 03:11:20 -0500

    Add login to router

--- Interactive Design Fundamentals - TC1015/dogmatic/dogmatic-app/src/index.js
+++ Interactive Design Fundamentals - TC1015/dogmatic/dogmatic-app/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
+
+import App from 'App'
+import { Login } from 'pages/Login'
+
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
+      <Route exact path="/login" component={Login} />
       <Route exact path="/" component={App} />
       <Route component={App} />
     </Switch>
