85ca9445df1be90fd97616bc63a72ad45e2fe4f5
Michael McQuade <giraffesyo@gmail.com>
2018-04-15 00:20:30 -0500

    Add reactstrap

--- Interactive Design Fundamentals - TC1015/dogmatic/dogmatic-app/src/index.js
+++ Interactive Design Fundamentals - TC1015/dogmatic/dogmatic-app/src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import 'bootstrap-css-only/css/bootstrap.min.css'
 import { Home } from 'components/Home'
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={App} />
+      <Route exact path="/" component={App} />
       <Route component={App} />
     </Switch>
   </BrowserRouter>,
