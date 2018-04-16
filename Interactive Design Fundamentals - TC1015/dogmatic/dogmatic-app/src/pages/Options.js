8bbd41d80729e0378d77160b5945758c0e0ebb75
Michael McQuade <giraffesyo@gmail.com>
2018-04-16 04:34:13 -0500

    Functional logout

--- Interactive Design Fundamentals - TC1015/dogmatic/dogmatic-app/src/pages/Options.js
+++ Interactive Design Fundamentals - TC1015/dogmatic/dogmatic-app/src/pages/Options.js
@@ -1,10 +1,56 @@
 import React from 'react'
 
-class Options extends React.PureComponent {
+import { Container, Row, Col } from 'reactstrap'
+import { Header } from 'components/Header'
+import { BottomNav } from 'components/BottomNav'
+import { Link, Redirect } from 'react-router-dom'
+import localForage from 'localforage'
 
-  render()
-  {
-    return null
+
+class Options extends React.PureComponent {
+  state = { name: '', username: '', loggedIn: true }
+
+  signout = () => {
+    localForage.setItem('currentUser', '').then(this.setState({loggedIn: false}))
+  }
+
+  render() {
+    const { name, username, loggedIn } = this.state
+    const { signout } = this
+    return (
+      <div>
+        <Header />
+        <Container style={{ marginLeft: '5vw', color: 'rgb(26, 154, 189)' }}>
+          <Row style={{ marginTop: '10vh' }}>
+            <Col>Name: {name}</Col>
+          </Row>
+          <Row>
+            <Col>E-mail: {username}</Col>
+          </Row>
+          <Row>
+            <Col>
+              <Link style={{ color: 'rgb(26, 154, 189)' }} to={'/about'}>
+                About Us
+              </Link>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Link style={{ color: 'rgb(26, 154, 189)' }} to={'/feedback'}>
+                Give us feedback
+              </Link>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <span onClick={signout}>Sign Out</span>
+            </Col>
+          </Row>
+        </Container>
+        <BottomNav />
+        {loggedIn ? null : <Redirect to={'/'}></Redirect>}
+      </div>
+    )
   }
 }
 
