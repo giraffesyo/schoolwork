3dcc410dba9ef28a2bc21016fec1f0e39fb6c3db
Michael McQuade <giraffesyo@gmail.com>
2018-05-01 03:13:18 -0500

    App.js

--- Analysis and Design of Algorithms/project-evaluation-problem/react-app/src/App.js
+++ Analysis and Design of Algorithms/project-evaluation-problem/react-app/src/App.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react'
+import { Container, Row, Col, Alert } from 'reactstrap'
+
+import { AddExpert } from 'components/AddExpert'
+import { AddProject } from 'components/AddProject'
+import 'App.css'
 
 class App extends Component {
+  state = { experts: [], projects: [], message: '', err: '' }
+
+  addExpert = expert => {
+    const { experts } = this.state
+    this.setState({ experts: [...experts, expert] })
+  }
+
+  addProject = project => {
+    const { projects } = this.state
+    this.setState({ projects: [...projects, project] })
+  }
+
   render() {
-    return <div>Hello World</div>
+    const { addExpert, addProject } = this
+    const { message, err } = this.state
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Alert color={err}>{message}</Alert>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <AddExpert addExpert={addExpert} />
+          </Col>
+          <Col md={6}>
+            <AddProject addProject={addProject} />
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
 
