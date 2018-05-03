d04e5311eaf32cd3c2792c466cac5ae5ec1fab1e
Michael McQuade <giraffesyo@gmail.com>
2018-05-03 02:08:13 -0500

    Working google-analytics

--- Interactive Design Fundamentals - TC1015/dogmatic/dogmatic-app/src/App.js
+++ Interactive Design Fundamentals - TC1015/dogmatic/dogmatic-app/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Router, Route, Switch } from 'react-router-dom'
-import createBrowserHistory from 'history/createBrowserHistory'
-import ReactGA from 'react-ga'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { NotLoggedIn } from 'pages/NotLoggedIn'
 import { Login } from 'pages/Login'
 import { Register } from 'pages/Register'
@@ -12,39 +10,28 @@ import { About } from 'pages/About'
 import { Feedback } from 'pages/Feedback'
 import { AddBowl } from 'pages/AddBowl'
 import { DeleteDog } from 'pages/DeleteDog'
-
-ReactGA.initialize('UA-44000145-3')
-const history = createBrowserHistory()
-history.listen((location, action) => {
-  //console.log(location.pathname)
-  ReactGA.set({ page: location.pathname })
-  ReactGA.pageview(location.pathname)
-})
+import GoogleAnalytics from './components/GoogleAnalytics'
 
 class App extends React.PureComponent {
-  componentDidMount() {
-    //console.log(history.location.pathname)
-    ReactGA.set({ page: history.location.pathname })
-    ReactGA.pageview(history.location.pathname)
-  }
-
   render() {
     return (
-      <Router history={history} basename="/dogmatic/app">
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/deletedog" component={DeleteDog} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/mybowls" component={Mybowls} />
-          <Route exact path="/options" component={Options} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/feedback" component={Feedback} />
-          <Route exact path="/addbowl" component={AddBowl} />
-          <Route exact path="/" component={NotLoggedIn} />
-          <Route component={NotLoggedIn} />
-        </Switch>
-      </Router>
+      <BrowserRouter basename="/dogmatic/app">
+        <GoogleAnalytics>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/deletedog" component={DeleteDog} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/mybowls" component={Mybowls} />
+            <Route exact path="/options" component={Options} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/feedback" component={Feedback} />
+            <Route exact path="/addbowl" component={AddBowl} />
+            <Route exact path="/" component={NotLoggedIn} />
+            <Route component={NotLoggedIn} />
+          </Switch>
+        </GoogleAnalytics>
+      </BrowserRouter>
     )
   }
 }
