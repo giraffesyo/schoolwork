c55d40d2f56c238c4db6833e662eeffb56b4a39a
Michael McQuade <giraffesyo@gmail.com>
2018-08-30 10:08:35 -0500

    Initialize in class

---
+++ Mobile App Development/in-class/Aug30/Aug30/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Aug30
+//
+//  Created by Michael McQuade on 8/30/18.
+//  Copyright © 2018 Michael McQuade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelGoPokes: UILabel!
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
