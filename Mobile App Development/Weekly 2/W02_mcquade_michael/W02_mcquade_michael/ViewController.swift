16b5cec5cb176d3509c056b0b84af3284bd0581f
Michael McQuade <giraffesyo@gmail.com>
2018-09-01 03:18:16 -0500

    Create view

--- Mobile App Development/Weekly 2/W02_mcquade_michael/W02_mcquade_michael/ViewController.swift
+++ Mobile App Development/Weekly 2/W02_mcquade_michael/W02_mcquade_michael/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
-
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let tappedNumber: Int = Int(sender.titleLabel!.text!)!
+        displayLabel.text = sender.titleLabel!.text!
+    }
+    
 }
+
 
