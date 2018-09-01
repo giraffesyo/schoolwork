ae0e9ba74167aaaca28e23689a014688ac4f0471
Michael McQuade <giraffesyo@gmail.com>
2018-08-31 22:59:44 -0500

    Initialize weekly 2

---
+++ Mobile App Development/Weekly 2/W02_mcquade_michael/W02_mcquade_michael/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  W02_mcquade_michael
+//
+//  Created by Michael McQuade on 8/31/18.
+//  Copyright © 2018 Michael McQuade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
