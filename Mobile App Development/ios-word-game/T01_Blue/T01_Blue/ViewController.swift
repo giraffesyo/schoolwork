4bcde68ab23c6fc44247d76a4600ca5c4769190f
Josh Sheridan <joshsheridan@users-macbook-pro-6.local>
2018-09-19 20:48:31 -0500

    created display for the app. Set up outlets and actions for all labels, image views, and buttons.

--- Mobile App Development/ios-word-game/T01_Blue/T01_Blue/ViewController.swift
+++ Mobile App Development/ios-word-game/T01_Blue/T01_Blue/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Outlets
+    @IBOutlet weak var scoreDisplay: UILabel! //score above the puzzle
+    @IBOutlet weak var strikeOne: UILabel! //first X
+    @IBOutlet weak var strikeTwo: UILabel! //second X
+    @IBOutlet weak var strikeThree: UILabel! //third X
+    @IBOutlet weak var pointsDisplay: UILabel! //points in the selector box
+    @IBOutlet weak var oneOne: UIImageView! //row one, index one
+    @IBOutlet weak var oneTwo: UIImageView! //row one, index two
+    @IBOutlet weak var twoOne: UIImageView! //row two, index one
+    @IBOutlet weak var twoTwo: UIImageView! //row two, index two
+    @IBOutlet weak var twoThree: UIImageView! //row two, index three
+    @IBOutlet weak var twoFour: UIImageView! //row two, index four
+    @IBOutlet weak var twoFive: UIImageView! //row two, index five
+    
+    //class variables
+    let points:[Int] = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 20, 20, 20, 20, 20, 20, 30, 30, 30, 30, 30, 40, 40, 40, 50, 50, 50, 100, 100, 250, 500]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        strikeOne.isHidden = true
+        strikeTwo.isHidden = true
+        strikeThree.isHidden = true
     }
 
-
+    // Called whenever the lever is pressed, starting off the process of randomly getting a point value, and then choosing a letter
+    @IBAction func leverPressed(_ sender: UIButton) {
+        let selectedPoints = points.randomElement()
+        //pointsDisplay.text = String(selectedPoints)
+        
+        //figure out how to have keyboard pop up for user entry. Store user entered letter
+    }
+    
+    //create function to check if keyboard entry matches any of the puzzle pieces
+    
 }
 
