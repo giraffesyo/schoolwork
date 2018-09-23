c68b96852a85496c6033ffadc7bef37448869bec
Josh McMahan <josh@joshmcmahan.com>
2018-09-22 23:38:00 -0500

    Create splash screen

---
+++ Mobile App Development/ios-word-game/T01_Blue/T01_Blue/SplashScreen.swift
@@ -0,0 +1,33 @@
+//
+//  SplashScreen.swift
+//  T01_Blue
+//
+//  Created by Joshua B McMahan on 9/22/18.
+//  Copyright © 2018 Team Blue. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "startGame", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
