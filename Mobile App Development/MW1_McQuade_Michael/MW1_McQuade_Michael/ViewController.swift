33de715a35c06cf9cbe610965e72546208638892
Michael McQuade <github@giraffesyo.io>
2018-09-27 20:10:12 -0500

    Initialize multiweekly one

---
+++ Mobile App Development/MW1_McQuade_Michael/MW1_McQuade_Michael/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MW1_McQuade_Michael
+//
+//  Created by Michael E McQuade on 9/27/18.
+//  Copyright © 2018 giraffesyo.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
