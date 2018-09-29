bfcefe452e04e5d437fac5fd946f84f2576eebe5
Michael McQuade <github@giraffesyo.io>
2018-09-28 20:30:42 -0500

    Implement first lap timing

--- Mobile App Development/MW1_McQuade_Michael/MW1_McQuade_Michael/Stopwatch.swift
+++ Mobile App Development/MW1_McQuade_Michael/MW1_McQuade_Michael/Stopwatch.swift
@@ -11,39 +11,57 @@ import UIKit
 
 class Stopwatch: NSObject {
     
-    private var laps: Int
-    private var time: Double
-    private var formatter: DateFormatter
+    // laps will store the amount of time each lap took, measured in deciseconds
+    private var laps: [Int]
+    private var formatter: DateComponentsFormatter
     
     override init() {
-        self.laps = 0
-        self.time = 0
-        self.formatter = DateFormatter.init()
+        self.laps = []
+        //Documentation for allowed units at https://developer.apple.com/documentation/foundation/datecomponentsformatter
+        self.formatter = DateComponentsFormatter.init()
+        formatter.allowsFractionalUnits = false
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
         
     }
     
-    func getLaps() -> Int {
-        return laps
+    func getLapCount() -> Int {
+        return laps.count
     }
     
-    func getTime() -> Double {
-        return time
+    func getLapTimeInDeciseconds(lap: Int) -> Int {
+        return self.laps[lap]
     }
     
-    /*func getFormattedTime() -> String {
-        
-    }*/
-    
-    //Sets the time and returns the previous time
-    func setTime(time: Double) -> Double {
-        let oldTime = self.time
-        self.time = time
-        return oldTime
+    func start() {
+        laps.append(0)
     }
     
-    // incrememnts laps by one and returns new lap amount
-    func incrementLap() -> Int {
-        self.laps = self.laps + 1
-        return self.laps
+    //Sets the lap time for given lap
+    func updateLapTime(lap: Int, time: Int) -> Void {
+        self.laps[lap] = time
     }
+    
+    //Increment time by 1 decisecond
+    func incrementLapTimeByDecisecond(lap: Int) -> Void {
+        let currentTime = self.laps[lap]
+        updateLapTime(lap: lap, time: currentTime + 1)
+    }
+    
+    //Returns a formatted time string given a lap number
+    func getFormattedTimeForLap(lap: Int) -> String {
+        let time = self.laps[lap]
+        return getFormattedTimeFromDeciseconds(time: time)
+    }
+    
+    //Returns a formatted string given deciseconds
+    private func getFormattedTimeFromDeciseconds(time: Int) -> String {
+        //Divide time by 10 to get time in seconds
+        let timeWithoutDeciseconds = formatter.string(from: TimeInterval(time/10))!
+        let deciseconds = String(time % 10)
+        return "\(timeWithoutDeciseconds).\(deciseconds)"
+    }
+
+
 }
--- Mobile App Development/MW1_McQuade_Michael/MW1_McQuade_Michael/TimerViewController.swift
+++ Mobile App Development/MW1_McQuade_Michael/MW1_McQuade_Michael/TimerViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    
+    @IBOutlet var lapNumberLabel: UILabel!
+    @IBOutlet var LapTimeLabel: UILabel!
+    @IBOutlet var TotalLapTimeLabel: UILabel!
+    
+    //create instance of our stopwatch class
+    var stopwatch: Stopwatch = Stopwatch()
+    //create a timer which we will use to update our stopwatch
+    var timer: Timer = Timer.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    @IBAction func StartStopButtonPressed(_ sender: UIButton) {
+        //escape if for some reason the title label isn't there
+        guard let nextState = sender.titleLabel!.text else {
+            print("nextState returned nil, starting watch")
+            start(startbutton: sender)
+            return
+        }
+        //call the appropriate function based on current state
+        switch(nextState){
+        case "START":
+            start(startbutton: sender)
+            break
+        case "STOP":
+            stop(stopbutton: sender)
+            break
+        default:
+            print("Default case was reached within StartStopButtonPressed function, value of nextState is \(nextState)")
+            break
+        }
+    }
+    
+    func start(startbutton: UIButton) {
+        //start the stopwatch if its our first lap
+        if stopwatch.getLapCount() == 0{
+            stopwatch.start()}
+        startbutton.setTitle("STOP", for: UIControl.State.normal)
+        startbutton.setTitleColor(UIColor.red, for: UIControl.State.normal)
 
-
+        let currentLapIndex = stopwatch.getLapCount() - 1
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: {_ in
+            self.stopwatch.incrementLapTimeByDecisecond(lap: currentLapIndex)
+            self.LapTimeLabel.text = self.stopwatch.getFormattedTimeForLap(lap: currentLapIndex)
+        })
+        lapNumberLabel.text = String(stopwatch.getLapCount())
+        
+    }
+    
+    func stop(stopbutton: UIButton) {
+        //stop the timer
+        timer.invalidate()
+        stopbutton.setTitle("START", for: UIControl.State.normal)
+        stopbutton.setTitleColor(UIColor.green, for: UIControl.State.normal)
+    }
+    
 }
 
