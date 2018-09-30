fc01fde0d61c3416983bba3e81b5113b49159a4f
Michael McQuade <github@giraffesyo.io>
2018-09-29 21:38:41 -0500

    Associate Lap Times View Controller to class

---
+++ Mobile App Development/MW1_McQuade_Michael/MW1_McQuade_Michael/LapTimesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LapTimesViewController.swift
+//  MW1_McQuade_Michael
+//
+//  Created by Michael E McQuade on 9/29/18.
+//  Copyright © 2018 giraffesyo.io. All rights reserved.
+//
+
+import UIKit
+
+class LapTimesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
