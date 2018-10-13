464bd4b34d8bd3d01ad69089b124f5d923c2b2d8
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 18:26:13 -0500

    Format code

--- Hackathons/GlobalHack VII/help2behelped/client/src/pages/Home.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/Home.js
@@ -4,11 +4,12 @@ import localForage from 'localforage'
 import { Link } from 'react-router-dom'
 
 class Home extends React.PureComponent {
-
-  
-
   render() {
-    return <div><Link to="/home">Click here to login</Link></div>
+    return (
+      <div>
+        <Link to="/home">Click here to login</Link>
+      </div>
+    )
   }
 }
 
