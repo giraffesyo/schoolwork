57b928a7fe33a8f1fea64f529b4e83b5897eb04b
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 17:11:06 -0500

    Add link to login

--- Hackathons/GlobalHack VII/help2behelped/client/src/pages/Home.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import localForage from 'localforage'
 
+import { Link } from 'react-router-dom'
+
 class Home extends React.PureComponent {
 
   
 
   render() {
-    return <div>Hello World helo hello</div>
+    return <div><Link to="/home">Click here to login</Link></div>
   }
 }
 
