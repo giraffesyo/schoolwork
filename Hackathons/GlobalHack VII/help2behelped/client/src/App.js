a4d5680a5b30d32e271b21f495e4febc2fd46398
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 17:07:15 -0500

    Use hashrouter

--- Hackathons/GlobalHack VII/help2behelped/client/src/App.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 import { LoggedInHome } from './pages/LoggedInHome'
@@ -7,13 +7,13 @@ import { LoggedInHome } from './pages/LoggedInHome'
 class App extends React.PureComponent {
   render() {
     return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/home" component={LoggedInHome} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/" component={Home} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     )
   }
 }
