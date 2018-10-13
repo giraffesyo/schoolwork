a7b7a856d6a1775c8167562d99d5239122646525
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 18:26:24 -0500

    Show mock shouts

--- Hackathons/GlobalHack VII/help2behelped/client/src/pages/LoggedInHome.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/LoggedInHome.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Nav } from '../blocks/Nav'
+import { Shouts } from '../components/Shouts'
+
+
+
 
 class LoggedInHome extends React.PureComponent {
   render() {
     return (
       <div>
         <Nav />
+        <Shouts />
       </div>
     )
   }
