d0b55a078d17a31408fa9f2a3edb981bbee61c75
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 14:47:28 -0500

    Add login page

--- Hackathons/GlobalHack VII/help2behelped/client/src/App.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
+import { Login } from './pages/Login'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route exact path="/login" component={Login} />
           <Route exact path="/" component={Home} />
         </Switch>
       </BrowserRouter>
--- Hackathons/GlobalHack VII/help2behelped/client/src/pages/Login.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/Login.js
@@ -1,10 +1,80 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const CenteredDiv = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+const StyledLogin = styled.div`
+  padding: 1rem;
+  button {
+    color: white;
+    padding: 0.5rem;
+    border-radius: 5%;
+    margin-left: auto;
+    margin-right: 0.25rem;
+    min-width: 45%;
+  }
+`
+
+const LoginButton = styled.button`
+  background-color: black;
+`
+
+const RegisterButton = styled.button`
+  background-color: green;
+`
+
+const InputGroup = styled.div`
+  width: 50%;
+  margin: 0.5rem 0;
+  input {
+    padding: 0.5rem;
+  }
+`
+const ForgotPasswordButton = styled.button`
+  background-color: #fff;
+  border: 0;
+  color: blue !important;
+`
 
 class Login extends React.PureComponent {
-render() {
-  return null
-}
+  handleSignUp = data => {
+    console.log(data)
+  }
+  handleLogin = data => {
+    console.log(data)
+  }
+
+  handleForgotPassword = data => {
+    console.log('we are clicking the forget password')
+    console.log(data)
+  }
+
+  render() {
+    const { handleSignUp, handleLogin, handleForgotPassword } = this
+    return (
+      <CenteredDiv>
+        <h1>Help 2 Be Helped</h1>
+        <StyledLogin>
+          <InputGroup>
+            <input placeholder="username" />
+          </InputGroup>
+          <InputGroup>
+            <input placeholder="password" />
+          </InputGroup>
+          <RegisterButton onClick={handleSignUp}>Register</RegisterButton>
+          <LoginButton onClick={handleLogin}>Login</LoginButton>
+          <ForgotPasswordButton onClick={handleForgotPassword}>
+            Forgot password?
+          </ForgotPasswordButton>
+        </StyledLogin>
+      </CenteredDiv>
+    )
+  }
 }
 
-
-export { Login } 
+export { Login }
