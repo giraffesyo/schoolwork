3b2a86077ae4dea5bab0e918916e25a807f10196
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 15:12:57 -0500

    Add route to home page

--- Hackathons/GlobalHack VII/help2behelped/client/src/App.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
+import { LoggedInHome } from './pages/LoggedInHome'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route exact path="/home" component={LoggedInHome} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/" component={Home} />
         </Switch>
