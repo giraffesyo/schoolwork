59801e9296b187916cb177d38dceecc3e111210b
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 21:43:30 -0500

    bootstrap example

--- Hackathons/GlobalHack VII/help2behelped/client/src/pages/Search.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/Search.js
@@ -2,9 +2,23 @@ import React from 'react'
 import Layout from '../HOC/Layout'
 
 class Search extends React.PureComponent {
-
   render() {
-    return <Layout>Search</Layout>
+    return (
+      <Layout>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>Hello world</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h2>this is html...</h2>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
   }
 }
 
