dbd411ef2ac64ae73d0fe5b139edb984df1b60ed
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 21:15:20 -0500

    Remove google translate

--- Hackathons/GlobalHack VII/help2behelped/client/src/HOC/Layout.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/HOC/Layout.js
@@ -3,26 +3,12 @@ import { Nav } from '../blocks/Nav'
 import styled from 'styled-components'
 import '../overrides.css'
 
-const Translate = styled.div`
-  margin-right: 1rem;
-  img { 
-    display: none;
-  }
-  text-align: right;
-  & > div {
-    color: transparent;
-  }
-  & > div > :not(:nth-child(1)) {
-    display: none !important;
-    content: '';
-  }
-`
+
 
 class Layout extends React.PureComponent {
   render() {
     return (
       <>
-        <Translate id="google_translate_element" />
         {this.props.children}
         <Nav />
       </>
