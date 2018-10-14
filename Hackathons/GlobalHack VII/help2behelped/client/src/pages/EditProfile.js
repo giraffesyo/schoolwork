fe6349d85c7825ada8e6ad4a81019a5c9c331afb
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2018-10-14 04:05:07 -0500

    added to edit

--- Hackathons/GlobalHack VII/help2behelped/client/src/pages/EditProfile.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/EditProfile.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import Layout from '../HOC/Layout'
 import { ProfileTopNav } from '../blocks/ProfileTopNav'
+import {FaEdit}from 'react-icons/fa'
+import styled from 'styled-components'
+import { bold } from 'ansi-colors';
+
+
+const StyleName = styled.div`
+font-size: medium;
+text-align: right;
+margin: .5rem;
+`
+const BioInfo = styled.div`
+text-align: center;
+border: solid;
+max-width: 45%;
+float: right;
+font-size: small;
+`
+
+
 
 class EditProfile extends React.PureComponent {
   render() {
@@ -9,12 +28,15 @@ class EditProfile extends React.PureComponent {
         <div className="container">
           <div className="row">
             <div className="col">
-              <h1>Hello world</h1>
+              <StyleName>Giraffesyo <FaEdit /></StyleName>
+            </div>
+            <div ClassName="col">
+            <BioInfo>Hi! I'm a senior programmer at OSU and bla bla bla</BioInfo>
             </div>
           </div>
           <div className="row">
             <div className="col">
-              <h2>this is html...</h2>
+              <h2></h2>
             </div>
           </div>
         </div>
