82c00ebb8d4a539fdd23a5cf96c0f9cedb357526
Michael McQuade <giraffesyo@gmail.com>
2018-10-14 07:15:05 -0500

    Fix bottom padding

--- Hackathons/GlobalHack VII/help2behelped/client/src/data/shouts.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/data/shouts.js
@@ -8,5 +8,25 @@ export default [
     received: 'Hangout',
     price: '1',
     distance: '0.8'
+  },
+  {
+    received: 'Tutoring',
+    price: '1',
+    distance: '0.3'
+  },
+  {
+    received: 'Math help',
+    price: '2',
+    distance: '1.8'
+  },
+  {
+    received: 'help moving',
+    price: '1',
+    distance: '3.8'
+  },
+  {
+    received: 'help with groceries',
+    price: '1',
+    distance: '0.5'
   }
 ]
--- Hackathons/GlobalHack VII/help2behelped/client/src/pages/LoggedInHome.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/LoggedInHome.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Layout from '../HOC/Layout'
 import { Shouts } from '../components/Shouts'
+import styled from 'styled-components'
 
+const BigBottomMargin = styled.div`
+  margin-bottom: 90px;
+`
 class LoggedInHome extends React.PureComponent {
   render() {
     return (
       <Layout>
-        <Shouts />
+        <BigBottomMargin>
+
+          <Shouts />
+        </BigBottomMargin>
       </Layout>
     )
   }
