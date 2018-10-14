ea31c6f473862634c15aedd0c494c475ce58e269
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 20:31:39 -0500

    Add translate

--- Hackathons/GlobalHack VII/help2behelped/client/src/HOC/Layout.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/HOC/Layout.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Nav } from '../blocks/Nav'
+import styled from 'styled-components'
+
+const Translate = styled.div`
+  text-align: right;
+  & > div {
+    color: transparent;
+  }
+  & > div > :not(:nth-child(1)) {
+    display: none !important;
+    content: '';
+  }
+`
 
 class Layout extends React.PureComponent {
   render() {
     return (
       <>
+        <Translate id="google_translate_element" />
         {this.props.children}
         <Nav />
       </>
