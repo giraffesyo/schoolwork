d357d5e64b0a2947ae6a5068db122eee00a6bc59
Michael McQuade <giraffesyo@gmail.com>
2018-10-14 03:05:23 -0500

    Space away from content

--- Hackathons/GlobalHack VII/help2behelped/client/src/HOC/Layout.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/HOC/Layout.js
@@ -3,12 +3,17 @@ import { Nav } from '../blocks/Nav'
 import styled from 'styled-components'
 import '../overrides.css'
 
+const Wrapper = styled.div`
+  margin: 100px 0;
+`
 
 class Layout extends React.PureComponent {
   render() {
+    const { TopNav } = this.props
     return (
       <>
-        <div className="container">{this.props.children}</div>
+        {!!TopNav && <TopNav />}
+        <Wrapper className="container">{this.props.children}</Wrapper>
         <Nav />
       </>
     )
