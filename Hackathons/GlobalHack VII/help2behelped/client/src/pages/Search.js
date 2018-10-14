02c2e7652028e278249161fd55a745e469a2fbda
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 19:18:50 -0500

    Start search page

---
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../HOC/Layout'
+
+class Search extends React.PureComponent {
+
+  render() {
+    return <Layout>Test</Layout>
+  }
+}
+
+export { Search }
