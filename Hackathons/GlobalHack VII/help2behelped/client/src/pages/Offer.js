1b1c87dd1fa9482eb56c142f6a984fdec751b137
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2018-10-14 02:20:49 -0500

    Add styling and content for Offer page

--- Hackathons/GlobalHack VII/help2behelped/client/src/pages/Offer.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/Offer.js
@@ -1,9 +1,87 @@
 import React from 'react'
 import Layout from '../HOC/Layout'
+import styled from 'styled-components'
+import { gray, green, purple } from '../colors'
+import { FaCaretUp } from 'react-icons/fa'
+
+
+
+const Box = styled.div`
+  background-color: ${gray};
+  flex-basis: 1;
+  border-radius: 25px;
+  margin: 1rem;
+  padding: 1rem;
+  position: relative;
+`
+const Description = styled.div`
+  font-size: 1rem;
+  padding: 0.5rem 0;
+  font-weight: 700;
+  text-align: center;
+  color: ${green};
+`
+const Purple = styled.span`
+  color: ${purple};`
+
+const OfferStyles = styled.div`
+.top_nav{
+background: #BDE4D2;
+padding: 10px;
+text-decoration-color: #BDE4D2;
+
+
+
+}
+
+`
 
 class Offer extends React.PureComponent {
   render() {
-    return <Layout>Offer</Layout>
+    return(
+        <Layout> 
+        <OfferStyles>
+        <div className="top_nav">
+            <div className="row">
+            <div className="col">
+                    <u>Profile</u>
+                </div>
+                <div className="col">
+                    <u>Messages</u>
+                </div>
+                <div className="col">
+                    <u>Edit</u>
+                </div>
+            </div>
+        </div>
+
+        <Box>
+        <Description>
+          Desh wants to teach you {' '}
+          <Purple>
+             Spanish <br/>
+          </Purple>
+          <small>Hi! I was born in Mexico and speak fluent english.</small>
+        </Description>
+      </Box>
+
+      <Box>
+        <Description>
+          Jake wants to do some <Purple>Shopping</Purple> for you<br/>{' '}
+          <small>Hey! I'm from Germany, but can only speak a little English.</small>
+        </Description>
+      </Box>
+
+      <Box>
+        <Description>
+          Zach wants to help you with <Purple>Math</Purple><br/>{' '}
+          <small>Hi everyone, I just moved here from Ecuador. I can speak both Spanish and English. </small>
+        </Description>
+      </Box>
+        
+        
+      </OfferStyles>
+        </Layout>)
   }
 }
 
