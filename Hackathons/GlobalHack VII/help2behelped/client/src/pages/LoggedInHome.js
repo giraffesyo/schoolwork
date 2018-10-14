1e6a5639236cec641dc75c9202b6b7c332c3efd9
Michael McQuade <giraffesyo@gmail.com>
2018-10-14 07:54:52 -0500

    Redirect if not logged in

--- Hackathons/GlobalHack VII/help2behelped/client/src/pages/LoggedInHome.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/LoggedInHome.js
@@ -2,18 +2,31 @@ import React from 'react'
 import Layout from '../HOC/Layout'
 import { Shouts } from '../components/Shouts'
 import styled from 'styled-components'
+import { Redirect } from 'react-router-dom'
+import localForage from 'localforage'
 
 const BigBottomMargin = styled.div`
   margin-bottom: 90px;
 `
 class LoggedInHome extends React.PureComponent {
+  state = { loaded: false, loggedIn: false }
+  async componentDidMount() {
+    const user = await localForage.getItem('user')
+    if (!!user) {
+      this.setState({ loaded: true, loggedIn: true })
+    } else {
+      this.setState({ loaded: true, loggedIn: false })
+    }
+  }
+
   render() {
+    const { loaded, loggedIn } = this.state
     return (
       <Layout>
         <BigBottomMargin>
-
           <Shouts />
         </BigBottomMargin>
+        {loaded && !loggedIn && <Redirect to={'/'} />}
       </Layout>
     )
   }
