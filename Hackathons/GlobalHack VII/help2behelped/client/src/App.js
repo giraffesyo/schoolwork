470419829f059f346117ae86dad26ed14c83b3ec
Michael McQuade <giraffesyo@gmail.com>
2018-10-14 03:04:52 -0500

    Update router with new routes

--- Hackathons/GlobalHack VII/help2behelped/client/src/App.js
+++ Hackathons/GlobalHack VII/help2behelped/client/src/App.js
@@ -6,13 +6,16 @@ import { LoggedInHome } from './pages/LoggedInHome'
 import { Offer } from './pages/Offer'
 import { Settings } from './pages/Settings'
 import { Search } from './pages/Search'
-
+import { EditProfile } from './pages/EditProfile'
+import { Messages } from './pages/Messages'
 
 class App extends React.PureComponent {
   render() {
     return (
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
+          <Route exact path="/editprofile" component={EditProfile} />
+          <Route exact path="/messages" component={Messages} />
           <Route exact path="/search" component={Search} />
           <Route exact path="/offer" component={Offer} />
           <Route exact path="/settings" component={Settings} />
