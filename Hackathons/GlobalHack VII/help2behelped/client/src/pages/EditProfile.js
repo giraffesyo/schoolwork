c178d1b680dbdb1e62d2a5e6d36eb8415c3365b6
Michael McQuade <giraffesyo@gmail.com>
2018-10-14 03:04:49 -0500

    Add new page components

---
+++ Hackathons/GlobalHack VII/help2behelped/client/src/pages/EditProfile.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Layout from '../HOC/Layout'
+import { ProfileTopNav } from '../blocks/ProfileTopNav'
+
+class EditProfile extends React.PureComponent {
+  render() {
+    return (
+      <Layout TopNav={ProfileTopNav}>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>Hello world</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h2>this is html...</h2>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export { EditProfile }
