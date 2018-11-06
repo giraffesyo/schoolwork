9bd678f0cbb198a4e8f9d51671752a9da6cfd186
Michael McQuade <giraffesyo@gmail.com>
2018-11-05 19:14:13 -0600

    Start weekly 11

---
+++ Mobile App Development/W11_McQuade_Michael/app/src/main/java/io/giraffesyo/w11_mcquade_michael/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.giraffesyo.w11_mcquade_michael
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
