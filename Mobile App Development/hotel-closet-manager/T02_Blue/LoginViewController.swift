5d00ea6bd3e52c78bc44a4d9ca73d5ccdafdee76
Saptami Biswas <sabiswa@mac03-cs.ms.okstate.edu>
2018-11-11 18:05:35 -0600

    Added inventory closet view controller and updated the login page

--- Mobile App Development/hotel-closet-manager/T02_Blue/LoginViewController.swift
+++ Mobile App Development/hotel-closet-manager/T02_Blue/LoginViewController.swift
@@ -15,14 +15,17 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var forgotPasswordButton: UIButton!
     @IBOutlet weak var createNewAccount: UIButton!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var timer = Timer.self
+    var secondsRemaining = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.isHidden = true
         self.navigationController!.navigationBar.isHidden = true
         userIdTextField.delegate = self
         passwordTextField.delegate = self
-
-        // Do any additional setup after loading the view.
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -33,14 +36,36 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func loginButtonAction(_ sender: UIButton)
+    {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer: Timer) -> Void in
+            self.activityIndicator.isHidden = false
+            self.secondsRemaining -= 1
+            self.activityIndicator.startAnimating()
+            
+            if self.secondsRemaining == 0 {
+                
+                timer.invalidate()
+                self.activityIndicator.stopAnimating()
+                self.performSegue(withIdentifier: "segueToInventoryFromLogin", sender: self)
+                
+            }
+        })
+        
+        /*In this function oyther details are to be added. That is if the entered email-id and password is not there in the database then it would show an alert to the user.*/
+        
+        //if incorrect email-id or password, then
+        // call showAlert()
+        
+        //showAlert()
     }
-    */
-
+    
+    @IBAction func showAlert()
+    {
+        let alert = UIAlertController(title: "Oops", message: "The entered email-id and password does not exist. Please try again if you already have an account. If you are a new user then please register.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
