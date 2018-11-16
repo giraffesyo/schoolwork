95f09a13d5db5ff291bc96ed10d1515fc88c7482
Josh Sheridan <joshsheridan@users-macbook-pro-6.local>
2018-11-15 18:16:08 -0600

    reworked Forgot Password page

--- Mobile App Development/hotel-closet-manager/T02_Blue/ForgotPasswordViewController.swift
+++ Mobile App Development/hotel-closet-manager/T02_Blue/ForgotPasswordViewController.swift
@@ -6,68 +6,62 @@
 //  Copyright © 2018 Josh Sheridan. All rights reserved.
 //
 
+// TODO: overwrite the password that matches the entered email with the new entered password and store the new one in the database
+
 import UIKit
 
 class ThirdViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var enterCodeTextField: UITextField!
-    @IBOutlet weak var remainingTime: UILabel!
-    @IBOutlet weak var sendCodeButton: UIButton!
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var verifyButton: UIButton!
-    
-    var remainingSeconds = 60
-    //var timer: Timer.self
+    @IBOutlet weak var securityQuestionView: UIView!
+    @IBOutlet weak var newPasswordView: UIView!
+    @IBOutlet weak var enterEmail: UITextField!
+    @IBOutlet weak var securityQuestionDisplay: UILabel!
+    @IBOutlet weak var securityAnswer: UITextField!
+    @IBOutlet weak var newPassword: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController!.navigationBar.isHidden = false
-        
+        securityQuestionView.isHidden = true
+        newPasswordView.isHidden = true
     }
     
-    @IBAction func sendCodeAction(_ sender: UIButton) {
-
-        sendCodeButton.isEnabled = false
-        enterCodeTextField.delegate = self
-        remainingTime.text = "01:00"
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer: Timer) -> Void in
-            // Decrement the seconds that remain.
-            self.remainingSeconds -= 1
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField === enterEmail {
             
-            if self.remainingSeconds == 0 {
-                timer.invalidate()
-                self.showAlert()
-                self.sendCodeButton.isEnabled = true
-                self.sendCodeButton.setTitle("Resend Code", for: .normal)
-                self.sendCodeButton.backgroundColor = UIColor.red
-                self.remainingSeconds = 60
-                
-            }
+            //TODO: add logic to check value with values in the database, and move on if email is validated
             
-            if(self.remainingSeconds == 60)
-            {
-                self.remainingTime.text = "01:00"
-            }
-            else
-            {
-                self.remainingTime.text = "00:" + String(self.remainingSeconds)
-            }
-            })
-        //send a randomly generated 6-digit number to the registered email or mobile number
-        
+            //TODO: retrieve security question and answer from database that match the entered email
+            
+            securityQuestionView.isHidden = false
+            return true
+        }
+        else if textField === securityAnswer {
+            
+            //TODO: check security answer with retrieved answer from database
+            
+            newPasswordView.isHidden = false
+            return true
+        }
+        else if textField === newPassword {
+            
+            //TODO: store the new password in a local variable
+            
+            return true
+        }
+        else if textField === confirmPassword {
+            
+            //TODO: check to ensure confirm password text matches with what was entered in new password
+            
+            return true
+        }
+        return true
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
-    }
-    
-    @IBAction func showAlert()
-    {
-        let alert = UIAlertController(title: "Timed out!", message: "If you did not recieve an email, try again by hitting the resend button", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
