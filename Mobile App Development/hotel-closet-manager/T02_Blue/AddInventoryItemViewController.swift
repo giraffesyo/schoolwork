ffbd54902d4cae01e55aab8e8ca354d13eb417c0
JoshMcMahan <github@joshmcmahan.com>
2018-11-25 01:34:39 -0600

    Made it so that the keyboards on the add inventory screen can be closed

--- Mobile App Development/hotel-closet-manager/T02_Blue/AddInventoryItemViewController.swift
+++ Mobile App Development/hotel-closet-manager/T02_Blue/AddInventoryItemViewController.swift
@@ -8,15 +8,51 @@
 
 import UIKit
 
-class AddInventoryItemViewController: UIViewController {
 
+
+class AddInventoryItemViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var addItemName: UITextField!
+    @IBOutlet weak var addInventoryAmount: UITextField!
+    @IBOutlet weak var addThresholdAmount: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.addItemName.delegate = self
+        self.addInventoryAmount.delegate = self
+        self.addThresholdAmount.delegate = self
+        
+        self.addDoneButtonOnKeyboard()
         // Do any additional setup after loading the view.
     }
     
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        doneToolbar.barStyle       = UIBarStyle.default
+        let flexSpace              = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem  = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: #selector(doneButtonAction))
+        
+        var items = [UIBarButtonItem]()
+        items.append(flexSpace)
+        items.append(done)
+        
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.addInventoryAmount.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction()
+    {
+        self.addInventoryAmount.resignFirstResponder()
+    }
+    
     /*
     // MARK: - Navigation
 
