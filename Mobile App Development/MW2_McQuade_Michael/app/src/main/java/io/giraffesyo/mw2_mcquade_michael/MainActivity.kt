d4c5b887b640bc8ec012e9cafdeaf3e0d9278a86
Michael McQuade <giraffesyo@gmail.com>
2018-12-02 00:01:10 -0600

    Setup of basic recycler view

--- Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/MainActivity.kt
+++ Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/MainActivity.kt
@@ -2,11 +2,30 @@ package io.giraffesyo.mw2_mcquade_michael
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val states: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Load the array
+        addStates()
+
+        // Create a vertical Layout Manager and add it to our recycler view
+        rvStates.layoutManager = LinearLayoutManager(this)
+
+        // Set the adapter for our recycler view
+        rvStates.adapter = StatesRecyclerViewAdapter(states, this)
+    }
+
+    fun addStates() {
+        for(i in 1..8) {
+            states.add("aardvark $i")
+        }
     }
 }
--- Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/StatesRecyclerViewAdapter.kt
+++ Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/StatesRecyclerViewAdapter.kt
@@ -2,29 +2,30 @@ package io.giraffesyo.mw2_mcquade_michael
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.recyclerview_row.view.*
 
 class StatesRecyclerViewAdapter(val items: ArrayList<String>, val context: Context): RecyclerView.Adapter<StatesRecyclerViewAdapter.ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.recyclerview_row, p0, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return items.size
+    }
+    // p0 will be each cell, p1 is the index
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        // Set the state name in the current cell's (p0) view holder
+        // the text is coming from items.get(p1), which returns the item at index p1
+        p0?.tvStateName?.text = items.get(p1)
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-
         // Assign a reference to the text view which will hold the state name
         val  tvStateName = view.tvStateName
-
-
     }
 }
 
