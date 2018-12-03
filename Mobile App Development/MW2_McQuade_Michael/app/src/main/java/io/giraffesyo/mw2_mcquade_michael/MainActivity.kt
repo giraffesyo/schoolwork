0f2fb385f071c5deefe16b519ef3ce652050ff90
Michael McQuade <giraffesyo@gmail.com>
2018-12-03 05:48:20 -0600

    Complete the layout

--- Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/MainActivity.kt
+++ Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/MainActivity.kt
@@ -1,13 +1,15 @@
 package io.giraffesyo.mw2_mcquade_michael
 
+import android.content.res.TypedArray
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    val states: ArrayList<String> = ArrayList()
+    // declare a container for our states
+    val states: ArrayList<Triple<Drawable?,String, String>> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +26,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun addStates() {
-        for(i in 1..8) {
-            states.add("aardvark $i")
+        val StateNames: Array<String> = resources.getStringArray(R.array.state_names)
+        val StateNickNames: Array<String> = resources.getStringArray(R.array.state_nicknames)
+        val StateImages: TypedArray =  resources.obtainTypedArray(R.array.state_images)
+        // we have 50 states so we will do a loop that will execute 50 times to create our array
+        for (i in 0..49){
+            // get the current state out of our state.xml file
+            val img: Drawable? = StateImages.getDrawable(i)
+            val name: String = StateNames[i]
+            val nickname: String = StateNickNames[i]
+            // create a triplet with the above values
+            var state = Triple(img, name, nickname )
+            // add the state to our array of triplets
+            states.add(i, state)
         }
+        // Clean up our typed array
+        StateImages.recycle()
     }
 }
--- Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/StatesRecyclerViewAdapter.kt
+++ Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/StatesRecyclerViewAdapter.kt
@@ -1,15 +1,27 @@
 package io.giraffesyo.mw2_mcquade_michael
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.recyclerview_row.view.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
+import org.jetbrains.anko.image
+import org.jetbrains.anko.info
 
-class StatesRecyclerViewAdapter(val items: ArrayList<String>, val context: Context): RecyclerView.Adapter<StatesRecyclerViewAdapter.ViewHolder>() {
+class StatesRecyclerViewAdapter(val items: ArrayList<Triple<Drawable?, String, String>>, val context: Context): RecyclerView.Adapter<StatesRecyclerViewAdapter.ViewHolder>(), AnkoLogger {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.recyclerview_row, p0, false))
+        val viewHolder =  ViewHolder(LayoutInflater.from(context).inflate(R.layout.recyclerview_row, p0, false))
+        info("test")
+        info(viewHolder.itemView.id)
+        viewHolder.itemView.setOnClickListener{
+            info("Clicked on cell $p1")
+        }
+
+        return viewHolder
     }
 
     override fun getItemCount(): Int {
@@ -19,13 +31,17 @@ class StatesRecyclerViewAdapter(val items: ArrayList<String>, val context: Conte
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
         // Set the state name in the current cell's (p0) view holder
         // the text is coming from items.get(p1), which returns the item at index p1
-        p0?.tvStateName?.text = items.get(p1)
+        p0.tvStateName?.text = items.get(p1).second
+        p0.tvStateCaption?.text = items.get(p1).third
+        p0.ivStateImage?.image = items.get(p1).first
     }
 
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         // Assign a reference to the text view which will hold the state name
-        val  tvStateName = view.tvStateName
+        val tvStateName = view.tvStateName
+        val ivStateImage = view.ivStateImage
+        val tvStateCaption = view.tvStateCaption
     }
 }
 
