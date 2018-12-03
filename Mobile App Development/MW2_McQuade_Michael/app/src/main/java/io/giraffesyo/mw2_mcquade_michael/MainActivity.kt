6107d5685c359adafbb6f1c3b2346b303290821a
Michael McQuade <giraffesyo@gmail.com>
2018-12-03 07:53:40 -0600

    Add scoring

--- Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/MainActivity.kt
+++ Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/MainActivity.kt
@@ -9,7 +9,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     // declare a container for our states
-    val states: ArrayList<Triple<Drawable?,String, String>> = ArrayList()
+    val states: ArrayList<US_State> = ArrayList()
+    // create an array of "already seen" flags
+    val alreadySeen: BooleanArray = BooleanArray(50)
+    // create a score
+    var score: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +26,35 @@ class MainActivity : AppCompatActivity() {
         rvStates.layoutManager = LinearLayoutManager(this)
 
         // Set the adapter for our recycler view
-        rvStates.adapter = StatesRecyclerViewAdapter(states, this)
+        rvStates.adapter = StatesRecyclerViewAdapter(states, ::changeScore, this)
     }
+
+    fun changeScore(by: Int){
+        // increment (or decrement if by is negative) by the given argument
+        this.score += by
+        // set the text view to have the score displayed
+        tvScore.text = "Score: ${score.toString()}/50"
+    }
+
 
     fun addStates() {
         val StateNames: Array<String> = resources.getStringArray(R.array.state_names)
         val StateNickNames: Array<String> = resources.getStringArray(R.array.state_nicknames)
-        val StateImages: TypedArray =  resources.obtainTypedArray(R.array.state_images)
+        val StateImages: TypedArray = resources.obtainTypedArray(R.array.state_images)
         // we have 50 states so we will do a loop that will execute 50 times to create our array
-        for (i in 0..49){
+        for (i in 0..49) {
             // get the current state out of our state.xml file
             val img: Drawable? = StateImages.getDrawable(i)
             val name: String = StateNames[i]
             val nickname: String = StateNickNames[i]
-            // create a triplet with the above values
-            var state = Triple(img, name, nickname )
-            // add the state to our array of triplets
+            // create an object with the above values
+            var state = US_State(img, name, nickname, false)
+            // add the state to our array of states
             states.add(i, state)
         }
         // Clean up our typed array
         StateImages.recycle()
     }
+
+    class US_State(val img: Drawable?, val name: String, val nickname: String, var seen: Boolean)
 }
--- Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/StatesRecyclerViewAdapter.kt
+++ Mobile App Development/MW2_McQuade_Michael/app/src/main/java/io/giraffesyo/mw2_mcquade_michael/StatesRecyclerViewAdapter.kt
@@ -1,47 +1,92 @@
 package io.giraffesyo.mw2_mcquade_michael
 
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.recyclerview_row.view.*
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.debug
-import org.jetbrains.anko.image
-import org.jetbrains.anko.info
+import org.jetbrains.anko.*
 
-class StatesRecyclerViewAdapter(val items: ArrayList<Triple<Drawable?, String, String>>, val context: Context): RecyclerView.Adapter<StatesRecyclerViewAdapter.ViewHolder>(), AnkoLogger {
+class StatesRecyclerViewAdapter(val items: ArrayList<MainActivity.US_State>, val changeScore: (by: Int) -> Unit, val context: Context) :
+    RecyclerView.Adapter<StatesRecyclerViewAdapter.ViewHolder>(), AnkoLogger {
+
+
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        val viewHolder =  ViewHolder(LayoutInflater.from(context).inflate(R.layout.recyclerview_row, p0, false))
-        info("test")
-        info(viewHolder.itemView.id)
-        viewHolder.itemView.setOnClickListener{
-            info("Clicked on cell $p1")
-        }
-
+        val viewHolder = ViewHolder(LayoutInflater.from(context).inflate(R.layout.recyclerview_row, p0, false))
         return viewHolder
     }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        super.onBindViewHolder(holder, position, payloads)
+
+        if(items[position].seen){
+            // if seen we should have a customized pastel background color
+            holder.llStateRow.backgroundColor = this.context.resources.getColor(R.color.colorAlreadySeen)
+        } else {
+            // if not seen we should have a white background
+            holder.llStateRow.setBackgroundColor(Color.TRANSPARENT)
+        }
+
+        // handle click events by adding a listener to the view
+        holder.itemView.setOnClickListener{
+
+            info("clicked $position")
+            if(!items[position].seen){
+                // the user hadn't seen this state until now, so we need to mark it and increase the score!
+                holder.llStateRow.backgroundColor = this.context.resources.getColor(R.color.colorAlreadySeen)
+                items[position].seen = true
+                // increment the score by 1
+                changeScore(1)
+            }
+        }
+    }
+
+
+
+   /* // called when cell is clicked
+    override fun onClick(v: View?) {
+        info("clicked")
+
+        /*
+
+        val test
+            info("Clicked on")
+            // if we have haven't already seen the state, increment the score and change hte background color
+            if (!viewHolder.alreadySeen) {
+                viewHolder.llStateRow.backgroundColor = this.context.resources.getColor(R.color.colorAlreadySeen)
+            } else {
+                //otherwise, we have seen the state, ask the user if we want to remove the state
+                //viewHolder.alreadySeen
+            }
+         */
+    }*/
 
     override fun getItemCount(): Int {
         return items.size
     }
+
     // p0 will be each cell, p1 is the index
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
         // Set the state name in the current cell's (p0) view holder
         // the text is coming from items.get(p1), which returns the item at index p1
-        p0.tvStateName?.text = items.get(p1).second
-        p0.tvStateCaption?.text = items.get(p1).third
-        p0.ivStateImage?.image = items.get(p1).first
+        p0.tvStateName.text = items.get(p1).name
+        p0.tvStateCaption.text = items.get(p1).nickname
+        p0.ivStateImage.image = items.get(p1).img
     }
 
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         // Assign a reference to the text view which will hold the state name
-        val tvStateName = view.tvStateName
-        val ivStateImage = view.ivStateImage
-        val tvStateCaption = view.tvStateCaption
+        val tvStateName: TextView = view.tvStateName
+        val ivStateImage: ImageView = view.ivStateImage
+        val tvStateCaption: TextView = view.tvStateCaption
+        val llStateRow: LinearLayout = view.llStateRow
     }
 }
 
