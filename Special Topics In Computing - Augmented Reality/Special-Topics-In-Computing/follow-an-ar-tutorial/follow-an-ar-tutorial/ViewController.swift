67ea28b31724a03f2c4b26a6bd4f137f8435a9eb
Michael McQuade <giraffesyo@gmail.com>
2019-01-21 18:54:11 -0600

    Add basis for AR with debug

--- Special Topics In Computing - Augmented Reality/Special-Topics-In-Computing/follow-an-ar-tutorial/follow-an-ar-tutorial/ViewController.swift
+++ Special Topics In Computing - Augmented Reality/Special-Topics-In-Computing/follow-an-ar-tutorial/follow-an-ar-tutorial/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import ARKit // we need this to use AR features, otherwise our types won't be recognized (e.g. ARSCNView
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var sceneView: ARSCNView!
+    // config which holds the correspondence between the real world and the virtual 3d plane that we will be working with
+    let config = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // setup options that allow drawing debug info onto scene
+        // we're drawing the feature points and the 3d world's origin/coordinate system.
+        sceneView.debugOptions = [ ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+    
+        // start the sceneview with our configuration
+        sceneView.session.run(config)
     }
-
-
+    
+    
 }
 
