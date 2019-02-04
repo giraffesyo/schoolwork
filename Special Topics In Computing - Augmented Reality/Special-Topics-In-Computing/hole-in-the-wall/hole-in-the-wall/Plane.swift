0acd08a3eb53ea1fb16d147a2577d2963297a19a
Michael McQuade <giraffesyo@gmail.com>
2019-02-04 14:06:51 -0600

    Rework Planes class implementation

--- Special Topics In Computing - Augmented Reality/Special-Topics-In-Computing/hole-in-the-wall/hole-in-the-wall/Plane.swift
+++ Special Topics In Computing - Augmented Reality/Special-Topics-In-Computing/hole-in-the-wall/hole-in-the-wall/Plane.swift
@@ -17,18 +17,33 @@ class Plane: SCNNode {
     
     init(anchor: ARPlaneAnchor){
         super.init()
-        let width: CGFloat = CGFloat(anchor.extent.x)
-        let height: CGFloat = CGFloat(anchor.extent.z)
-        self.geometry = SCNPlane(width: width, height: height)
-        // set position of plane to be at center of the ARPlaneAnchor
-
-        self.simdPosition = anchor.center
+        self.updateGeometry(anchor: anchor)
+        self.updatePosition(anchor: anchor)
+        //self.simdPosition = anchor.center
         
         // the orientation is "up" in its coordinate space, which is normal to the plane,
         // rotate to make it parallel with the plane
         self.eulerAngles = SCNVector3(Double.pi/2, 0, 0)
         
     }
+    
+    func updateGeometry(anchor: ARPlaneAnchor){
+        // get the anchor's width and height information
+        let newWidth: CGFloat = CGFloat(anchor.extent.x)
+        let newHeight: CGFloat = CGFloat(anchor.extent.z)
+        // create a SCNPlane object with the width and height of the anchor
+        self.geometry = SCNPlane(width: newWidth, height: newHeight)
+        self.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        // If you don't render both sides and it ends up that the material is facing the other way
+        // then you will be left with nothing visible ( not super necessary but very useful
+        // to see what is going on
+        self.geometry?.firstMaterial?.isDoubleSided = true
+    }
+    
+    func updatePosition(anchor: ARPlaneAnchor)
+    {
+        // set the position of the plane to be at the position of the anchor
+        self.simdPosition = anchor.center    }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
--- Special Topics In Computing - Augmented Reality/Special-Topics-In-Computing/hole-in-the-wall/hole-in-the-wall/ViewController.swift
+++ Special Topics In Computing - Augmented Reality/Special-Topics-In-Computing/hole-in-the-wall/hole-in-the-wall/ViewController.swift
@@ -7,14 +7,110 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
-
+// make view controller a delegate so that we can access AR callbacks
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .vertical
+        sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        sceneView.session.run(configuration)
+        sceneView.delegate = self
+        
     }
-
-
+    
+    
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        // we should do nothing if we're not dealing with an anchor that can be cast to ARPlaneAnchor type
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        // create our planeNode using the transform information of the plane anchor
+        let planeNode = Plane(anchor: planeAnchor)
+        // add node
+        node.addChildNode(planeNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // again we should do nothing if we're not dealing with an anchor that can be cast to the appropriate ARPlaneAnchor type
+        guard let planeAnchor = anchor as? ARPlaneAnchor, let planeNode = node.childNodes.first as? Plane else {return}
+        planeNode.updateGeometry(anchor: planeAnchor)
+        planeNode.updatePosition(anchor: planeAnchor)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        // dont handle non plane nodes
+        guard anchor is ARPlaneAnchor else { return }
+        // clean up nodes that arent in use anymore
+        node.enumerateChildNodes {(node, _ ) in
+            node.removeFromParentNode()
+        }
+    }
+    
+    
+    
+    
+    //    // this is called when a node is added
+    //    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    //        // lets ignore all nodes that are added except those that are found
+    //        // by the vertical plane detection
+    //        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+    //        // at this point we can guarantee that planeAnchor is an indeed a vertical plane
+    //
+    //        // create a Plane instance with our plane anchor
+    //        let plane = Plane(anchor: planeAnchor)
+    //        // color the plane blue
+    //        // Add the plane to the scene
+    //        node.addChildNode(plane)
+    //        print("plane added")
+    //
+    //    }
+    //
+    //    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+    //        // we need to update our plane information with the new anchor information
+    //        // but, first lets make sure that we're dealing with a plane anchor
+    //        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+    //        guard let plane = node.childNodes[0] as? Plane else { return }
+    //
+    //        let newWidth: CGFloat = CGFloat(planeAnchor.extent.x)
+    //        let newHeight: CGFloat = CGFloat(planeAnchor.extent.z)
+    //        let newPosition: simd_float3 = planeAnchor.center
+    //
+    //        plane.geometry = SCNPlane(width: newWidth, height: newHeight)
+    //        plane.simdPosition = newPosition
+    //        print("plane updated")
+    //
+    //    }
+    //
+    //    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+    //        // dont handle non plane nodes
+    //        guard anchor is ARPlaneAnchor else { return }
+    //        // clean up nodes that arent in use anymore
+    //        node.enumerateChildNodes {(node, _ ) in
+    //            node.removeFromParentNode()
+    //        }
+    //    }
+    /*
+     func createHole(anchor: ARPlaneAnchor) -> SCNNode {
+     // we'll make this one meter wide and one meter high
+     let holeNode = SCNNode(geometry: SCNPlane(width: 100.0, height: 100.0))
+     // we'll put the hole at the center of the plane anchor we're passed in
+     
+     
+     // should be parallel to the plane
+     holeNode.eulerAngles = SCNVector3(Double.pi/2, 0, 0)
+     //set the material to the image of the hole
+     let holeImage = #imageLiteral(resourceName: "hole")
+     holeNode.geometry?.firstMaterial?.diffuse.contents = holeImage
+     
+     return holeNode
+     }*/
+    
+    
 }
 
