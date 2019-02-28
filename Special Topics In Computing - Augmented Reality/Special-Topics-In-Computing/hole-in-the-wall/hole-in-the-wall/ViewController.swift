a4b50e2977407ad6dd98579e8a4d5362961dcb61
Michael McQuade <giraffesyo@gmail.com>
2019-02-28 13:01:06 -0600

    Clean up unused portions of the code/comments

--- Special Topics In Computing - Augmented Reality/Special-Topics-In-Computing/hole-in-the-wall/hole-in-the-wall/ViewController.swift
+++ Special Topics In Computing - Augmented Reality/Special-Topics-In-Computing/hole-in-the-wall/hole-in-the-wall/ViewController.swift
@@ -11,10 +11,6 @@ import ARKit
 
 // make view controller a delegate so that we can access AR callbacks
 class ViewController: UIViewController, ARSCNViewDelegate {
-    // Redoing without the custom Plane class
-    //    var firstPlane: Plane?
-    var planeCount = 0 // just important so we can see when we have our first plane
-    var holeNode: SCNNode = SCNNode()
     var holeAdded: Bool = false
     
     @IBOutlet var sceneView: ARSCNView!
@@ -26,13 +22,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
         sceneView.session.run(configuration)
         sceneView.delegate = self
-        
-        // extract the holeNode from the scene and save it into our view controller  instance variable 'holeNode'
-        let scene : SCNScene =  SCNScene(named: "Assets.scnassets/hole.scn")!
-        holeNode = scene.rootNode.childNodes.first!
-        sceneView.scene = scene
-        holeNode.removeFromParentNode()
-    }
+}
     
     
     
@@ -66,37 +56,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         planeNode.position = SCNVector3(x,y,z)
         // roate the plane by 90 degrees because it is by default normal to the anchor, causing it to be horizontal
         planeNode.eulerAngles.x = -.pi/2
-        //
-        //        if(planeCount == 0) {
-        //
-        //            print("Found first plane at \(planeNode.position)")
-        //            self.firstPlane = planeNode
-        //            print("setting hole with mario's anchor node to position \(planeAnchor.center), camera position is \(sceneView.session.currentFrame!.camera.transform)")
-        //            //holeNode.simdPosition = planeAnchor.center
-        //
-        //
-        //
-        //
-        //
-        //            //holeNode.scale = SCNVector3(1, 1,1)
-        //
-        //
-        //            // when i add it to a child of the plane, it moves with the phone
-        //            planeNode.addChildNode(holeNode)
-        //
-        //            //when i add it as a child of the scene, it does not move
-        //            holeNode.simdTransform = planeAnchor.transform
-        //            // the hole will be positioned normal to the plan so we need to rotate it
-        //            //holeNode.eulerAngles = SCNVector3(-Double.pi, 0, 0)
-        //            //flip on z axis ( so that it mario is right side up)
-        //            holeNode.eulerAngles = SCNVector3(Double.pi,0,-Double.pi)
-        //            sceneView.scene.rootNode.addChildNode(holeNode)
-        //            print("adding hole with mario to scene")
-        //        }
-        //
-        // increment plane counter (not using this in current branch)
-        self.planeCount += 1
-        
         // add the planeNode we created to be a child of the node plane detection is managing
         node.addChildNode(planeNode)
     }
@@ -148,10 +107,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             print("tap wasnt on a plane")
             return
         }
-        
-        
-//         translation is in the last column of the local transform
-//        let translation = hitTestResult.worldTransform.columns.3
         // i dont believe the following unwrap is necessary since we guaranteed our hit test result previously, but guarding just in case and would be interesting to know what cases would make this nil
         guard let planeAnchor = hitTestResult.anchor as? ARPlaneAnchor else {
             print("Plane Anchor not found in hit test result")
@@ -159,32 +114,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         print("tap was on a detected plane" )
         
-        //        let anchor = ARAnchor(transform: hitTestResult.worldTransform)
-//        addHole(worldTransform: translation)
-//        hitTestResult.anchor.
-        guard let holeScene = SCNScene(named: "Assets.scnassets/hole.scn"), let holeNode2 = holeScene.rootNode.childNode(withName: "hole", recursively: false) else {
-            print("Failed to load hole node from hole scene")
-            return
-        }
         let translation = hitTestResult.localTransform.columns.3
-        let x = translation.x
-        let y = translation.y
-        let z = translation.z
-        print("Adding hole at (\(x),\(y),\(z)) ")
-
-        holeNode2.position = SCNVector3(x,y,z)
-        // child node added is normal to plane so we have to rotate it around x
-        holeNode2.eulerAngles.x = -.pi/2
-        //rotate entire hole around z by 180 degrees so that mario is face up
-        holeNode2.eulerAngles.z = .pi
-//
-        guard let planeNode = sceneView.node(for: planeAnchor) else {
-            print("not able to get plane node from anchor")
-            return
-        }
-        print("Adding hole to scene")
-        holeNode2.position = SCNVector3(x,y,z)
-        planeNode.addChildNode(holeNode2)
+        addHoleToScene(localTranslation: translation ,planeAnchor: planeAnchor)
     }
     
     
@@ -193,23 +124,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // Apple has a great write up on user control https://github.com/gao0122/ARKit-Example-by-Apple
     
     
-    func addHole( worldTransform translation: simd_float4){
-        // position our hole using the local translation and the anchor for the plane that was tapped
-        // the position vector is the fourth column of a transform matrix
-        guard let holeScene = SCNScene(named: "Assets.scnassets/hole.scn"), let holeNode2 = holeScene.rootNode.childNode(withName: "hole", recursively: false) else {
-            print("Failed to get hole node from hole scene")
+    func addHoleToScene( localTranslation translation: simd_float4, planeAnchor anchor: ARPlaneAnchor){
+        
+        guard let holeScene = SCNScene(named: "Assets.scnassets/hole.scn"), let holeNode = holeScene.rootNode.childNode(withName: "hole", recursively: false) else {
+            print("Failed to load hole node from hole scene")
             return
         }
         
         let x = translation.x
         let y = translation.y
         let z = translation.z
-        print("Adding mario at (\(x),\(y),\(z)) ")
         
-        holeNode2.position = SCNVector3(x,y,z)
+        holeNode.position = SCNVector3(x,y,z)
+        // child node added is normal to plane so we have to rotate it around x
+        holeNode.eulerAngles.x = -.pi/2
+        //rotate entire hole around z by 180 degrees so that mario is face up
+        holeNode.eulerAngles.z = .pi
+        //
+        guard let planeNode = sceneView.node(for: anchor) else {
+            print("not able to get plane node from anchor")
+            return
+        }
+        print("Adding hole at (\(x),\(y),\(z)) ")
         
-        sceneView.scene.rootNode.addChildNode(holeNode2)
-        
+        holeNode.position = SCNVector3(x,y,z)
+        planeNode.addChildNode(holeNode)
     }
     
 }
