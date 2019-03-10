ec782bf3ebe764e9bf03c285dd51a373d8212147
Carson Hogstad <carson.hogstad@okstate.edu>
2019-03-09 21:03:36 -0600

    Finished Zelda
    She now has jump, fall, attack, movement, and hitboxs for her sword and her own hurtbox

--- Video Game Development/legend-of-link/Assets/ZeldaScript.cs
+++ Video Game Development/legend-of-link/Assets/ZeldaScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ZeldaScript : MonoBehaviour {
 
 	// Use this for initialization
+	public int jumpHeight = 5;
+	public bool isJumping;
 	public Animator anim;
 	public BoxCollider2D zeldaHitbox;
 	public Rigidbody2D rb;
@@ -13,33 +15,46 @@ public class ZeldaScript : MonoBehaviour {
 	private Vector3 flip;
 	private bool directionRight = true;
 	private AnimatorClipInfo[] clipInfo;
+	public bool grounded = true;
+	public EdgeCollider2D edCol;
+
 	void Start () {
-		
+
 		clipInfo = anim.GetCurrentAnimatorClipInfo(0);
 		rb = this.GetComponent<Rigidbody2D> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
-		if (clipInfo [0].clip.name.Equals ("Landing")) {
-			
-		}
-		else if (Input.GetKey(KeyCode.DownArrow)) {
+		if (!grounded && rb.velocity.y<0){
+			swordAttackBox.enabled = false;
+			anim.Play("Falling");
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				transform.position += Vector3.right * speed * Time.deltaTime;
+				if (!directionRight) {
+					directionRight = true;
+					transform.RotateAround (transform.position, transform.up, 180f);
+				}
+			} else if (Input.GetKey (KeyCode.LeftArrow)) {
+				transform.position += Vector3.left * speed * Time.deltaTime;
+				if (directionRight) {
+					directionRight = false;
+					transform.RotateAround (transform.position, transform.up, 180f);
+				}
+			}
+
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
 			zeldaHitbox.enabled = false;
 			swordAttackBox.enabled = true;
 			anim.Play ("Attacking");
-		} 
-		else if (Input.GetKeyUp (KeyCode.DownArrow)) {
+		} else if (Input.GetKeyUp (KeyCode.DownArrow)) {
 			zeldaHitbox.enabled = true;
 			swordAttackBox.enabled = false;
 			anim.Play ("Idle");
-		}
-
-		else if(Input.GetKey(KeyCode.UpArrow)){
-			anim.Play("Jumping");
-			rb.AddForce (Vector3.up);
-
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			rb.AddForce(Vector2.up * jumpHeight);
+			grounded = false;
 		}
 		else if(Input.GetKey(KeyCode.RightArrow)){
 			transform.position += Vector3.right * speed * Time.deltaTime;
@@ -48,7 +63,7 @@ public class ZeldaScript : MonoBehaviour {
 				transform.RotateAround (transform.position, transform.up, 180f);
 			}
 			anim.Play("Running");
-			}
+		}
 		else if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.position += Vector3.left * speed * Time.deltaTime;
 			if (directionRight) {
@@ -59,12 +74,19 @@ public class ZeldaScript : MonoBehaviour {
 		} 
 
 		else {
-			anim.Play ("Idle");
+			swordAttackBox.enabled = false;
+			if (grounded)
+				anim.Play ("Idle");
 		}
 
 	}
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		Debug.Log("OnCollisionEnter2D");
+		if (col.otherCollider.Equals (edCol)) {
+			anim.Play ("Landing");
+			grounded = true;
+		}
 	}
+
 }
