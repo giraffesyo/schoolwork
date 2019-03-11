f3d3301247a8330ee04c65963e11d0b83d137acb
SamuelWood <samuel.r.wood@okstate.edu>
2019-03-10 20:18:02 -0500

    Sliced zelda at bottom instead of middle. More lvl 1

--- Video Game Development/legend-of-link/Assets/Scripts/CameraControl.cs
+++ Video Game Development/legend-of-link/Assets/Scripts/CameraControl.cs
@@ -5,19 +5,65 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public GameObject player;
+    public int timeSpeed;
 
     private Vector3 offset;
     private GameObject leftBound;
+    private Camera cam;
+    private Color baseColor;
+    private bool darker;
+    private Color currentColor;
 
     // Start is called before the first frame update
     void Start()
     {
+        darker = true;
+        cam = GetComponent<Camera>();
+        Color baseColor = new Color(0, 198, 255);
         offset = transform.position - player.transform.position;
+    }
+
+    private void Update()
+    {
+        /*
+        float delta = Time.deltaTime/100 * timeSpeed;
+        if (darker)
+        {
+            currentColor.r -= baseColor.r * delta;
+            currentColor.g -= baseColor.g * delta;
+            currentColor.b -= baseColor.b * delta;
+        }
+        else
+        {
+            currentColor.r += baseColor.r * delta;
+            currentColor.g += baseColor.g * delta;
+            currentColor.b += baseColor.b * delta;
+        }
+        cam.backgroundColor = currentColor;
+        if (currentColor.b <= 0.001)
+        {
+            darker = false;
+        }
+        else if (currentColor.b >= 253.999)
+        {
+            darker = true;
+        }
+        */
     }
 
     public void Adjust(Vector3 shift)
     {
         transform.position = transform.position + shift;
+        //ChangeBackground(shift);
+    }
+
+    public void ChangeBackground(Vector3 shift)
+    {
+        float delta = shift.x / cam.pixelWidth * timeSpeed;
+        currentColor.r = currentColor.r - baseColor.r / 10 * delta;
+        currentColor.g = currentColor.g - baseColor.g / 10 * delta;
+        currentColor.b = currentColor.b - baseColor.b / 10 * delta;
+        cam.backgroundColor = currentColor;
     }
 
     // Update is called once per frame
