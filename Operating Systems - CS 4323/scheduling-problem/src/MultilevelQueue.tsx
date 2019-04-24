550eaebd1ed7ed1eb9acec2291450108074471de
Michael McQuade <giraffesyo@gmail.com>
2019-04-24 03:43:23 -0500

    Display initial data

--- Operating Systems - CS 4323/scheduling-problem/src/MultilevelQueue.tsx
+++ Operating Systems - CS 4323/scheduling-problem/src/MultilevelQueue.tsx
@@ -7,10 +7,72 @@ interface MultilevelQueueProps {
   timeQuantum: number[]
 }
 
+interface MultilevelQueueState {
+  queue: Process[][]
+}
+
 export default class MultilevelQueue extends React.PureComponent<
-  MultilevelQueueProps
+  MultilevelQueueProps,
+  MultilevelQueueState
 > {
+  state: MultilevelQueueState = {
+    queue: [],
+  }
+
+  constructor(props: MultilevelQueueProps) {
+    super(props)
+    console.log(props)
+  }
+
   render() {
-    return <div />
+    const {
+      props: { initialProcessesToSchedule },
+    } = this
+    const headerLabels = ['Process', 'Burst Time', 'Arrival', 'Priority Queue']
+
+    return (
+      <div className="container mt-5">
+        <h1>Multilevel Queue</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              {headerLabels.map(label => (
+                <th key={'th-' + label}>{label}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {initialProcessesToSchedule.map(
+              ({
+                identifier,
+                priority,
+                burst,
+                arrival,
+                completionTime,
+                remainingTime,
+                getTurnaroundTime,
+                getWaitingTime,
+                current,
+              }) => (
+                <tr
+                  className={
+                    completionTime
+                      ? 'table-success'
+                      : current
+                      ? 'table-primary '
+                      : ''
+                  }
+                  key={identifier}>
+                  <td>{identifier}</td>
+                  <td>{burst}</td>
+                  <td>{arrival}</td>
+                  <td>{priority}</td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
