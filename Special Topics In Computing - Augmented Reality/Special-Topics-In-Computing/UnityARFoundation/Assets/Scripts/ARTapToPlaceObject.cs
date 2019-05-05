f662a100a93b9edf7bf862284d063d4814a35583
Michael McQuade <giraffesyo@gmail.com>
2019-05-05 00:11:42 -0500

    Placement indicator

---
+++ Special Topics In Computing - Augmented Reality/Special-Topics-In-Computing/UnityARFoundation/Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+    //pose describes position and rotation of a 3d point
+    private Pose placementPose;
+    // we only consider this true if hits array has one item in it
+    private bool placementPoseIsValid = false;
+
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+
+    void Start()
+    {
+        // Save reference to arOrigin on start
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        // We want to find out where the camera is pointing and find out if there
+        // is a position in virtual space that we can place an object
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if(placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+        // disable placement indicator
+    }
+
+    private void UpdatePlacementPose()
+    {
+        // AR Version of raycast but we're checking against REAL world surfaces
+        // We need to get the screen center to send raycast from
+        Vector3 screenScenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        arOrigin.Raycast(screenScenter, hits, TrackableType.Planes);
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            Vector3 cameraForward = Camera.current.transform.forward;
+            Vector3 cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    private void UpdatePlacementIndicator() {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+}
