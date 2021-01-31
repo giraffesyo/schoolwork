96ccd69bb726e7fe8bba2368543105a3ce1c118b
Michael McQuade <michael@giraffesyo.io>
2021-01-30 20:12:46 -0600

    Reset player speed when teleporting to spawn

--- Immersive Computing - COSC 6397/cosc6397-assignment-1/Assets/Scripts/SendToSpawn.cs
+++ Immersive Computing - COSC 6397/cosc6397-assignment-1/Assets/Scripts/SendToSpawn.cs
@@ -10,5 +10,8 @@ public class SendToSpawn : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         other.gameObject.transform.position = spawnPoint.position;
+        Rigidbody otherRb = other.gameObject.GetComponent<Rigidbody>();
+        otherRb.angularVelocity = new Vector3(0, 0, 0);
+        otherRb.velocity = new Vector3(0, 0, 0);
     }
 }
