5095b30a6e3a76526aa26fc0e8b2340a30add52d
Michael McQuade <michael@giraffesyo.io>
2021-01-30 20:25:31 -0600

    Add score and price text elements

---
+++ Immersive Computing - COSC 6397/cosc6397-assignment-1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public float score = 0.0f;
+    public float dogecoinPrice = 0.05f;
+
+    public Text textPrice;
+    public Text textScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
