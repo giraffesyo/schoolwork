03a404ea1f108c3944eac072c7a26ebd1595394a
Michael McQuade <michael@giraffesyo.io>
2021-02-13 21:56:14 -0600

    Implement rotation on slider

--- Immersive Computing - COSC 6397/cosc6397-assignment-2/Assets/Scripts/RotationSlider.cs
+++ Immersive Computing - COSC 6397/cosc6397-assignment-2/Assets/Scripts/RotationSlider.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RotationSlider : MonoBehaviour
 {
+    private Slider slider;
+    public GameObject gameobjectToRotate;
     // Start is called before the first frame update
+    private float previousValue;
+
     void Start()
     {
-        
+        slider = GetComponent<Slider>();
+
+        slider.onValueChanged.AddListener(OnSliderChanged);
+        // initialize previousValue
+        previousValue = this.slider.value;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnSliderChanged(float newValue)
     {
-        
+        float change = newValue - this.previousValue;
+        this.gameobjectToRotate.transform.Rotate(Vector3.right * change * 360);
+        this.previousValue = newValue;
     }
 }
