207fe82252530cb944d6e9a013e2fd56b636d39a
Michael McQuade <michael@giraffesyo.io>
2021-03-28 16:45:06 -0500

    Drawing initial teapot without lighting

--- Computer Graphics - COSC 6372/cosc6372-computer-graphics/HW4/src/main.cpp
+++ Computer Graphics - COSC 6372/cosc6372-computer-graphics/HW4/src/main.cpp
@@ -5,25 +5,27 @@ using namespace std;
 
 Gz gz;
 
-void draw(const char* fileName) {
+void draw(const char *fileName)
+{
 	ifstream fi("Tris.txt");
 	int nTri;
-	fi>>nTri;	//Number of triangles
+	fi >> nTri; //Number of triangles
 
-	gz.clear(GZ_COLOR_BUFFER | GZ_DEPTH_BUFFER);	//Clear frame buffer with background color
-													//Clear depth buffer with default depth
+	gz.clear(GZ_COLOR_BUFFER | GZ_DEPTH_BUFFER); //Clear frame buffer with background color
+												 //Clear depth buffer with default depth
 
 	GzVertex v;
 	GzVector n;
 	GzColor c;
-	gz.begin(GZ_TRIANGLES);		//Draw triangles
-	for (int i=0; i<nTri; i++)
-		for (int j=0; j<3; j++) {
-			fi>>v[X]>>v[Y]>>v[Z];
+	gz.begin(GZ_TRIANGLES); //Draw triangles
+	for (int i = 0; i < nTri; i++)
+		for (int j = 0; j < 3; j++)
+		{
+			fi >> v[X] >> v[Y] >> v[Z];
 			gz.addVertex(v);
-			fi>>n[X]>>n[Y]>>n[Z];
+			fi >> n[X] >> n[Y] >> n[Z];
 			gz.addNormal(n);
-			fi>>c[R]>>c[G]>>c[B]>>c[A];
+			fi >> c[R] >> c[G] >> c[B] >> c[A];
 			gz.addColor(c);
 		}
 	gz.end();
@@ -33,66 +35,63 @@ void draw(const char* fileName) {
 	gz.toImage().save(fileName);
 }
 
-int main() {
+int main()
+{
 	gz.initFrameSize(320, 240);
-	
-	gz.clearColor(GzColor(0, 0, 0));	//Background color: Black
-	gz.enable(GZ_DEPTH_TEST);			//Use depth test
-	gz.clearDepth(-100);				//Default depth: -100
-	gz.enable(GZ_LIGHTING);
 
-	gz.addLight(GzVector(0, -1, 0), GzColor(1, 0.1, 0.1));	//Red light direction (0, -1, 0)
-	gz.addLight(GzVector(-1, 0, 0), GzColor(0.1, 1, 0.1));	//Green light direction (-1, 0, 0)
-	gz.addLight(GzVector(0, 0, -1), GzColor(0.1, 0.1, 1));	//Blue light direction (0, 0, -1)
-	gz.material(0.1, 0.3, 0.6, 3);		//Ambient, diffuse, specular, spec power
+	gz.clearColor(GzColor(0, 0, 0)); //Background color: Black
+	gz.enable(GZ_DEPTH_TEST);		 //Use depth test
+	gz.clearDepth(-100);			 //Default depth: -100
+	// gz.enable(GZ_LIGHTING);
 
-	
-	
-	gz.shadeModel(GZ_GOURAUD);
-	
-	gz.lookAt(	10, 10, 10,	//Eye position 
-				0, 0, 0,	//Look at
-				0, 1, 0);	//Up vector
-	gz.orthographic(-2.4, 2.4,	//Left and Right
-					-1.8, 1.8,	//Bottom and Top
-					1, 10);	//zNear and zFar
+	// gz.addLight(GzVector(0, -1, 0), GzColor(1, 0.1, 0.1));	//Red light direction (0, -1, 0)
+	// gz.addLight(GzVector(-1, 0, 0), GzColor(0.1, 1, 0.1));	//Green light direction (-1, 0, 0)
+	// gz.addLight(GzVector(0, 0, -1), GzColor(0.1, 0.1, 1));	//Blue light direction (0, 0, -1)
+	// gz.material(0.1, 0.3, 0.6, 3);		//Ambient, diffuse, specular, spec power
+
+	// gz.shadeModel(GZ_GOURAUD);
+
+	gz.lookAt(10, 10, 10,	   //Eye position
+			  0, 0, 0,		   //Look at
+			  0, 1, 0);		   //Up vector
+	gz.orthographic(-2.4, 2.4, //Left and Right
+					-1.8, 1.8, //Bottom and Top
+					1, 10);	   //zNear and zFar
 	gz.translate(0, -1, 0);
 	draw("GouraudTeaPot1.bmp");
-	
-	gz.lookAt(	3, 3, 3,	//Eye position 
-				0, 0, 0,	//Look at
-				0, 1, 0);	//Up vector
-	gz.perspective(	50,				//Fovy
-					(GzReal)4/3,	//Aspect
-					1, 10);			//zNear and zFar
-	gz.rotate(45, 1, 0, 0);
-	gz.translate(0, -1, 0);
 
-	draw("GouraudTeaPot2.bmp");
+	// gz.lookAt(	3, 3, 3,	//Eye position
+	// 			0, 0, 0,	//Look at
+	// 			0, 1, 0);	//Up vector
+	// gz.perspective(	50,				//Fovy
+	// 				(GzReal)4/3,	//Aspect
+	// 				1, 10);			//zNear and zFar
+	// gz.rotate(45, 1, 0, 0);
+	// gz.translate(0, -1, 0);
 
-	
-	
-	gz.shadeModel(GZ_PHONG);
+	// draw("GouraudTeaPot2.bmp");
 
-	gz.lookAt(	10, 10, 10,	//Eye position 
-				0, 0, 0,	//Look at
-				0, 1, 0);	//Up vector
-	gz.orthographic(-2.4, 2.4,	//Left and Right
-					-1.8, 1.8,	//Bottom and Top
-					1, 10);	//zNear and zFar
-	gz.translate(0, -1, 0);
-	draw("PhongTeaPot1.bmp");
-	
-	gz.lookAt(	3, 3, 3,	//Eye position 
-				0, 0, 0,	//Look at
-				0, 1, 0);	//Up vector
-	gz.perspective(	50,				//Fovy
-					(GzReal)4/3,	//Aspect
-					1, 10);			//zNear and zFar
-	gz.rotate(45, 1, 0, 0);
-	gz.translate(0, -1, 0);
+	// gz.shadeModel(GZ_PHONG);
 
-	draw("PhongTeaPot2.bmp");
-		
+	// gz.lookAt(	10, 10, 10,	//Eye position
+	// 			0, 0, 0,	//Look at
+	// 			0, 1, 0);	//Up vector
+	// gz.orthographic(-2.4, 2.4,	//Left and Right
+	// 				-1.8, 1.8,	//Bottom and Top
+	// 				1, 10);	//zNear and zFar
+	// gz.translate(0, -1, 0);
+	// draw("PhongTeaPot1.bmp");
+
+	// gz.lookAt(	3, 3, 3,	//Eye position
+	// 			0, 0, 0,	//Look at
+	// 			0, 1, 0);	//Up vector
+	// gz.perspective(	50,				//Fovy
+	// 				(GzReal)4/3,	//Aspect
+	// 				1, 10);			//zNear and zFar
+	// gz.rotate(45, 1, 0, 0);
+	// gz.translate(0, -1, 0);
+
+	// draw("PhongTeaPot2.bmp");
+
 	return 0;
 }
