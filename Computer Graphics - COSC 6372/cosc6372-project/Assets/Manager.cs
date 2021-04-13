0396eb4a8569270f545bf80ca08d00a09ae3f033
Michael McQuade <michael@giraffesyo.io>
2021-04-13 13:04:28 -0500

    Get normalized animation time

--- Computer Graphics - COSC 6372/cosc6372-project/Assets/Manager.cs
+++ Computer Graphics - COSC 6372/cosc6372-project/Assets/Manager.cs
@@ -10,23 +10,45 @@ public class Manager : MonoBehaviour
     private float RightHandYPosition;
     public GameObject BallPrefab;
     private GameObject ball;
+    [SerializeField]
+    private Animator modelAnimator;
+    public float normalizedAnimationTime;
     void Start()
     {
         UpdateRightHandPosition();
         ball = Instantiate(BallPrefab);
 
         // slow down the engine
-        Time.timeScale = 0.2f;
+        Time.timeScale = 1.0f;
+        AnimatorStateInfo animatorStateInfo = modelAnimator.GetCurrentAnimatorStateInfo(0);
+        normalizedAnimationTime = animatorStateInfo.normalizedTime;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdateRightHandPosition();
+        AnimatorStateInfo animatorStateInfo = modelAnimator.GetCurrentAnimatorStateInfo(0);
+        normalizedAnimationTime = animatorStateInfo.normalizedTime;
     }
 
     void UpdateRightHandPosition()
     {
         RightHandYPosition = rightHand.position.y;
+    }
+}
+
+class Section
+{
+    public float startY;
+    public float endY;
+    public float durationMs;
+
+    Section(float startY, float endY, float durationMs)
+    {
+        this.startY = startY;
+        this.endY = endY;
+        this.durationMs = durationMs;
     }
 }
