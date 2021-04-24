f475a1c9b28a1ad37c6fa0d6b57a29addd4cc375
Michael McQuade <michael@giraffesyo.io>
2021-04-24 18:53:00 -0500

    Add burger components to fridge

--- Immersive Computing - COSC 6397/GME-Project2/OverTheMoon/Assets/Scripts/FridgeScript.cs
+++ Immersive Computing - COSC 6397/GME-Project2/OverTheMoon/Assets/Scripts/FridgeScript.cs
@@ -2,25 +2,116 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FoodCompoonentType
+{
+    FIRST_MEAT,
+    SECOND_MEAT,
+    TOP_BUN,
+    BOTTOM_BUN,
+    LETTUCE,
+    TOMATO,
+    CHEESE
+}
+
 public class FridgeScript : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject TopBunSpawnPoint;
+    private Transform TopBunSpawnPoint;
     [SerializeField]
-    private GameObject BottomBunSpawnPoint;
+    private Transform BottomBunSpawnPoint;
     [SerializeField]
-    private GameObject LettuceSpawnPoint;
+    private Transform LettuceSpawnPoint;
     [SerializeField]
-    private GameObject FirstMeatSpawnPoint;
+    private Transform FirstMeatSpawnPoint;
     [SerializeField]
-    private GameObject SecondMeatSpawnPoint;
+    private Transform SecondMeatSpawnPoint;
     [SerializeField]
-    private GameObject TomatoSpawnPoint;
+    private Transform TomatoSpawnPoint;
+    [SerializeField]
+    private Transform CheeseSpawnPoint;
+
+    [SerializeField]
+    private GameObject MeatPrefab;
+    [SerializeField]
+    private GameObject LettucePrefab;
+    [SerializeField]
+    private GameObject TopBunPrefab;
+    [SerializeField]
+    private GameObject BottomBunPrefab;
+    [SerializeField]
+    private GameObject TomatoPrefab;
+    [SerializeField]
+    private GameObject CheesePrefab;
+
+    // spawn all the food items at once
+    void SpawnAll()
+    {
+        foreach (FoodCompoonentType food in System.Enum.GetValues(typeof(FoodCompoonentType)))
+        {
+            Spawn(food);
+        }
+    }
+
+    private void Spawn(FoodCompoonentType food)
+    {
+        switch (food)
+        {
+            case FoodCompoonentType.FIRST_MEAT:
+                {
+                    GameObject obj = Instantiate(MeatPrefab, FirstMeatSpawnPoint.position, Quaternion.identity);
+                    obj.transform.eulerAngles = MeatPrefab.transform.eulerAngles;
+                    break;
+                }
+            case FoodCompoonentType.SECOND_MEAT:
+                {
+                    GameObject obj = Instantiate(MeatPrefab, SecondMeatSpawnPoint.position, Quaternion.identity);
+                    obj.transform.eulerAngles = MeatPrefab.transform.eulerAngles;
+                    break;
+                }
+            case FoodCompoonentType.BOTTOM_BUN:
+                {
+                    GameObject obj = Instantiate(BottomBunPrefab, BottomBunSpawnPoint.position, Quaternion.identity);
+                    obj.transform.eulerAngles = MeatPrefab.transform.eulerAngles;
+                    break;
+                }
+            case FoodCompoonentType.TOP_BUN:
+                {
+                    GameObject obj = Instantiate(TopBunPrefab, TopBunSpawnPoint.position, Quaternion.identity);
+                    obj.transform.eulerAngles = MeatPrefab.transform.eulerAngles;
+                    break;
+                }
+            case FoodCompoonentType.LETTUCE:
+                {
+                    GameObject obj = Instantiate(LettucePrefab, LettuceSpawnPoint.position, Quaternion.identity);
+                    obj.transform.eulerAngles = MeatPrefab.transform.eulerAngles;
+                    break;
+                }
+            case FoodCompoonentType.CHEESE:
+                {
+                    GameObject obj = Instantiate(CheesePrefab, CheeseSpawnPoint.position, Quaternion.identity);
+                    obj.transform.eulerAngles = MeatPrefab.transform.eulerAngles;
+                    break;
+                }
+            case FoodCompoonentType.TOMATO:
+                {
+                    GameObject obj = Instantiate(TomatoPrefab, TomatoSpawnPoint.position, Quaternion.identity);
+                    obj.transform.eulerAngles = MeatPrefab.transform.eulerAngles;
+                    break;
+                }
+            default:
+                {
+                    Debug.LogError("Fell to default case in Spawn food component of fridge script");
+                    break;
+                }
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        SpawnAll();
     }
 
     // Update is called once per frame
