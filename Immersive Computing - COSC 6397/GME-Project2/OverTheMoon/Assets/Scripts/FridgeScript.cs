7c2a92044c2f75250e5998a45908450da09bbcac
Michael McQuade <michael@giraffesyo.io>
2021-04-24 16:47:49 -0500

    Add sections to fridge, add hamburger asset

---
+++ Immersive Computing - COSC 6397/GME-Project2/OverTheMoon/Assets/Scripts/FridgeScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject TopBunSpawnPoint;
+    [SerializeField]
+    private GameObject BottomBunSpawnPoint;
+    [SerializeField]
+    private GameObject LettuceSpawnPoint;
+    [SerializeField]
+    private GameObject FirstMeatSpawnPoint;
+    [SerializeField]
+    private GameObject SecondMeatSpawnPoint;
+    [SerializeField]
+    private GameObject TomatoSpawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
