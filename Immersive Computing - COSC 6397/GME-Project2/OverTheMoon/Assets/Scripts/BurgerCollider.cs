eaed26764d978d878255580e3e62c456c1cbaf6b
Michael McQuade <michael@giraffesyo.io>
2021-04-24 23:52:17 -0500

    Add burger scripts from Sean's branch
    Co-authored-by: stwalsh4118 <40175938+stwalsh4118@users.noreply.github.com>

---
+++ Immersive Computing - COSC 6397/GME-Project2/OverTheMoon/Assets/Scripts/BurgerCollider.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurgerCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Colliding " + !other.gameObject.GetComponent<BurgerComponent>().isManipulated);
+        GameObject BurgerComponentObject = gameObject.transform.parent.gameObject;
+
+        if (other.gameObject.tag == "BurgerComponent"  
+            && (!other.gameObject.GetComponent<BurgerComponent>().isManipulated)
+            && BurgerComponentObject.GetComponent<BurgerComponent>().Burger != null)
+        {
+            Debug.Log("Burger Collision");
+            
+
+            if (BurgerComponentObject.GetComponent<BurgerComponent>().isTop)
+            {
+                other.gameObject.GetComponent<BurgerComponent>().Burger = BurgerComponentObject.GetComponent<BurgerComponent>().Burger;
+                BurgerComponentObject.GetComponent<BurgerComponent>().Burger.GetComponent<Burger>().Top = other.gameObject;
+                BurgerComponentObject.GetComponent<BurgerComponent>().Burger.GetComponent<Burger>().BurgerComponents.Add(other.gameObject);
+
+                BurgerComponentObject.GetComponent<BurgerComponent>().isTop = false;
+                other.gameObject.GetComponent<BurgerComponent>().isTop = true;
+                //other.gameObject.transform.parent = BurgerComponentObject.GetComponent<BurgerComponent>().Burger;
+                other.gameObject.transform.parent = BurgerComponentObject.transform;
+                other.gameObject.transform.localPosition = new Vector3(0, 1, 0);
+                Destroy(other.gameObject.GetComponent<Rigidbody>());
+                
+            }
+        }
+    }
+}
---
+++ Immersive Computing - COSC 6397/GME-Project2/OverTheMoon/Assets/Scripts/BurgerComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurgerComponent : MonoBehaviour
+{
+    public bool isTop = false;
+    public Transform Burger;
+    public bool isManipulated = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setManipulated()
+    {
+        isManipulated = !isManipulated;
+        Debug.Log("Change Manipulation State " + isManipulated);
+    }
+}
