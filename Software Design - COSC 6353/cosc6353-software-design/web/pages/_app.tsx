a41b899e2248e6def13bd82cbd80aa4547ed2dee
Michael McQuade <michael@giraffesyo.io>
2021-06-13 21:30:19 -0500

    Convert to typescript

---
+++ Software Design - COSC 6353/cosc6353-software-design/web/pages/_app.tsx
@@ -0,0 +1,8 @@
+// import App from "next/app";
+import type { AppProps } from 'next/app'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
