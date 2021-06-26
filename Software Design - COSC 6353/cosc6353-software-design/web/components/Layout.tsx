0ccdfbb91ed0aa2fb1a4da0811662a86e371f6a1
Quan Nguyen <quannhoang@gmail.com>
2021-06-25 22:07:04 -0500

    quote page edit

--- Software Design - COSC 6353/cosc6353-software-design/web/components/Layout.tsx
+++ Software Design - COSC 6353/cosc6353-software-design/web/components/Layout.tsx
@@ -1,15 +1,42 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
+
+const LoggedInLinks = [
+  { href: "/quote", name: "Get Quote" },
+  { href: "/profile", name: "Profile" },
+];
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Head>
         <title>Fuel Quotes App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      {children}
+      <div>
+        <nav className="bg-gray-900 py-2 text-white flex justify-between">
+          <div className="hidden sm:block"></div>
+          <div>
+            {LoggedInLinks.map(({ name, href }, index) => (
+              <Link key={`nav-link-${index}`} href={href}>
+                <a className="text-xl font-medium hover:opacity-80 p-4">
+                  {name}
+                </a>
+              </Link>
+            ))}
+          </div>
+          <div>
+            <Link href="/logout">
+              <a className="text-xl font-medium hover:opacity-80 mr-5">
+                Logout
+              </a>
+            </Link>
+          </div>
+        </nav>
+        {children}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- Software Design - COSC 6353/cosc6353-software-design/web/pages/quote.tsx
+++ Software Design - COSC 6353/cosc6353-software-design/web/pages/quote.tsx
@@ -1,137 +1,166 @@
 // import TestConnect from '../components/TestConnect'
+import { DateTime } from "luxon";
+import { useEffect } from "react";
+import { SetStateAction, useState } from "react";
 
-import { SetStateAction, useState } from 'react'
-import classNames from '../utils/classNames'
-
-
-
-const GetQuote = () => {
-    const [gallon, setGallon] = useState('')
-
-  return (
-    <div className='bg-fuel h-screen'>
-        <div className=' h-full container mx-auto flex justify-center items-center'>
-             <div className='w-full h-full sm:h-auto sm:w-1/2 md:1/3 bg-white  rounded-lg'>
-                <div className='p-10'>
-                    <div className='mt-8'>
-                    Fuel Quote Form
-                    <div className='mt-6'>
-                        <form className='space-y-6'>
-                            <div>
-                                <label 
-                                    htmlFor='gallon' 
-                                    className='block text-sm font-medium text-gray-700' >
-                                    Gallon
-                                </label>
-                                <div className='mt-1'>
-                                    <input 
-                                        value={gallon}
-                                        id='gallon'
-                                        name='gallon'
-                                        onChange={(e) => setGallon(e.currentTarget.value)} 
-                                        required
-                                        className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                                    >
-                                    </input>
-
-                                </div>
-                            </div>
-                            <div>
-                                <label 
-                                    htmlFor='deliveryAddress' 
-                                    className='block text-sm font-medium text-gray-700' >
-                                    Delivery Address
-                                </label>
-                                <div className='mt-1'>
-                                    <label 
-                                        className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                                    >
-                                    1800 Main Street, Houston, TX, 77083
-                                    </label>
-
-                                </div>
-                            </div>
-                            <div>
-                                <label 
-                                    htmlFor='deliveryDate' 
-                                    className='block text-sm font-medium text-gray-700' >
-                                    Delivery Date
-                                </label>
-                                <div className='mt-1'>
-                                    <label 
-                                        className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                                    >
-                                    06/23/2021
-                                    </label>
-
-                                </div>
-                            </div>
-                            <div>
-                                <button
-                                type='submit'
-                                className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                                >
-                                Get Quote
-                                </button>
-                            </div>
-                        </form>
-
-                    </div>
-                </div>
-                </div>
-            </div>
-
-            <div className=' w-full h-full sm:h-auto sm:w-1/2 md:1/3 bg-white rounded-lg'>
-                <div className='p-10'>
-                    <div className='mt-8'>
-                    Quote History
-                    <div className='mt-6 text-xs text-center'>
-                        <table className='table-fixed '>
-                            <thead>
-                                <tr>
-                                    <th className='w-1/6'>Date</th>
-                                    <th className='w-1/6'>Gallon</th>
-                                    <th className='w-1/6'>Delivery Date</th>
-                                    <th className='w-1/6'>Price per Gallon</th>
-                                    <th className='w-1/6'>Total Ammount due</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                        <td>06/23/2021</td>
-                                        <td>20</td>
-                                        <td>09/23/2021</td>
-                                        <td>2</td>
-                                        <td>40</td>
-                                </tr>
-                                <tr>
-                                        <td>10/07/2020</td>
-                                        <td>25</td>
-                                        <td>01/05/2021</td>
-                                        <td>2</td>
-                                        <td>50</td>
-                                </tr>
-                                <tr>
-                                        <td>02/28/2019</td>
-                                        <td>40</td>
-                                        <td>05/01/2019</td>
-                                        <td>1.5</td>
-                                        <td>60</td>
-                                </tr>
-                            </tbody>
-                        </table>
-
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-      
-  )
+interface ITableProps {
+  headers: string[];
+  rows: string[][];
 }
 
-export default GetQuote
+const Table: React.FC<ITableProps> = ({ headers, rows }) => {
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  {headers.map((header) => (
+                    <th
+                      scope="col"
+                      key={`th-${header}`}
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      {header}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {rows.map((row, index) => (
+                  <tr
+                    className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                    key={`row-${index}`}
+                  >
+                    {row.map((item, item_index) => (
+                      <td
+                        className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+                        key={`item-${index}-${item_index}`}
+                      >
+                        {item}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
+const GetQuote = () => {
+  const [gallon, setGallon] = useState("");
+  const [deliveryDate, setDeliveryDate] = useState("");
+  const [todayDate, setTodayDate] = useState("");
+  useEffect(() => {
+    const now = DateTime.now().plus({ days: 1 }).toSQLDate();
+    setTodayDate(now);
+  }, []);
 
+  return (
+    <div className="lg:h-screen bg-fuel">
+      <div className="flex h-full flex-col justify-center items-center lg:pt-5  w-full text-sm">
+        <div className="bg-white w-2/3 rounded-lg m-5 gap-y-20 text-">
+          <div className="p-10">
+            <div className="mt-8">
+              <span className="text-xl font-medium">Fuel Quote Form</span>
+              <div className="mt-6">
+                <form className="space-y-6">
+                  <div>
+                    <label
+                      htmlFor="gallon"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Gallon
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="number"
+                        value={gallon}
+                        id="gallon"
+                        name="gallon"
+                        onChange={(e) => setGallon(e.currentTarget.value)}
+                        required
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      ></input>
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="deliveryAddress"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Delivery Address
+                    </label>
+                    <div className="mt-1">
+                      <label className="appearance-none bg-gray-200 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        1800 Main Street, Houston, TX, 77083
+                      </label>
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="deliveryDate"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Delivery Date
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="date"
+                        value={deliveryDate}
+                        onChange={(e) => setDeliveryDate(e.currentTarget.value)}
+                        min={todayDate}
+                        placeholder={todayDate}
+                        required
+                      ></input>
+                    </div>
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Get Quote
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className=" bg-white w-2/3 rounded-lg m-5">
+          <div className="p-10">
+            <div className="mt-8">
+              <span className="text-xl font-medium">Quote History</span>
+              <div className="mt-6 text-xs text-center">
+                <Table
+                  headers={[
+                    "Date",
+                    "Gallon",
+                    "Delivery Date",
+                    "Price Per Gallon",
+                    "Total Ammount due",
+                  ]}
+                  rows={[
+                    ["06/23/2021", "20", "09/23/2021", "2", "40"],
+                    ["10/07/2020", "25", "01/25/2021", "2", "50"],
+                    ["02/28/2019", "40", "05/01/2019", "1.5", "60"],
+                  ]}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GetQuote;
