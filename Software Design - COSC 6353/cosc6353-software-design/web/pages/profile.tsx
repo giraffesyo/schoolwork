9f7287fc021f76998807158089363583c2715243
Gal Egozi <galegozi@microsoft.com>
2021-06-26 23:30:36 -0500

    Fixed the profile

--- Software Design - COSC 6353/cosc6353-software-design/web/pages/profile.tsx
+++ Software Design - COSC 6353/cosc6353-software-design/web/pages/profile.tsx
@@ -16,14 +16,67 @@ const ProfileManagement = () => {
         // TODO: Set UI to loading state
     }
 
-    const getStates = () => {
-        // TODO: Access states from backend.
-        return [
-            "TX",
-            "US",
-            "IL"
-        ];
-    }
+    const states = {
+        AL: "Alabama",
+        AK: "Alaska",
+        AS: "American Samoa",
+        AZ: "Arizona",
+        AR: "Arkansas",
+        CA: "California",
+        CO: "Colorado",
+        CT: "Connecticut",
+        DE: "Delaware",
+        DC: "District Of Columbia",
+        FM: "Federated States Of Micronesia",
+        FL: "Florida",
+        GA: "Georgia",
+        GU: "Guam",
+        HI: "Hawaii",
+        ID: "Idaho",
+        IL: "Illinois",
+        IN: "Indiana",
+        IA: "Iowa",
+        KS: "Kansas",
+        KY: "Kentucky",
+        LA: "Louisiana",
+        ME: "Maine",
+        MH: "Marshall Islands",
+        MD: "Maryland",
+        MA: "Massachusetts",
+        MI: "Michigan",
+        MN: "Minnesota",
+        MS: "Mississippi",
+        MO: "Missouri",
+        MT: "Montana",
+        NE: "Nebraska",
+        NV: "Nevada",
+        NH: "New Hampshire",
+        NJ: "New Jersey",
+        NM: "New Mexico",
+        NY: "New York",
+        NC: "North Carolina",
+        ND: "North Dakota",
+        MP: "Northern Mariana Islands",
+        OH: "Ohio",
+        OK: "Oklahoma",
+        OR: "Oregon",
+        PW: "Palau",
+        PA: "Pennsylvania",
+        PR: "Puerto Rico",
+        RI: "Rhode Island",
+        SC: "South Carolina",
+        SD: "South Dakota",
+        TN: "Tennessee",
+        TX: "Texas",
+        UT: "Utah",
+        VT: "Vermont",
+        VI: "Virgin Islands",
+        VA: "Virginia",
+        WA: "Washington",
+        WV: "West Virginia",
+        WI: "Wisconsin",
+        WY: "Wyoming"
+    };
 
     return (
         <div className='mt-8'>
@@ -42,7 +95,7 @@ const ProfileManagement = () => {
                                 value={name}
                                 id='name'
                                 name='name'
-                                onChange={(e)=>setName(e.currentTarget.value)}
+                                onChange={(e) => setName(e.currentTarget.value)}
                                 required
                                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                             />
@@ -61,7 +114,7 @@ const ProfileManagement = () => {
                                 value={addr1}
                                 id='addr1'
                                 name='addr1'
-                                onChange={(e)=>setAddr1(e.currentTarget.value)}
+                                onChange={(e) => setAddr1(e.currentTarget.value)}
                                 required
                                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                             />
@@ -80,7 +133,7 @@ const ProfileManagement = () => {
                                 value={addr2}
                                 id='addr2'
                                 name='addr2'
-                                onChange={(e)=>setAddr2(e.currentTarget.value)}
+                                onChange={(e) => setAddr2(e.currentTarget.value)}
                                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                             />
                         </div>
@@ -98,30 +151,28 @@ const ProfileManagement = () => {
                                 value={city}
                                 id='city'
                                 name='city'
-                                onChange={(e)=>setCity(e.currentTarget.value)}
+                                onChange={(e) => setCity(e.currentTarget.value)}
                                 required
                                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                             />
                         </div>
                     </div>
 
-                    <div className='space-y-1'>
+                    <div>
                         <label
                             htmlFor='state'
                             className='block text-sm font-medium text-gray-700'
                         >
-                            State TODO: proper selector
+                            State
                         </label>
-                        <div className='mt-1'>
-                            <input
-                                value={state}
-                                id='state'
-                                name='state'
-                                onChange={(e)=>setState(e.currentTarget.value)}
-                                required
-                                className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                            />
-                        </div>
+                        <select onChange={(e) => setState(e.currentTarget.value)} required>
+                            <option value=""></option>
+                            {Object.keys(states).map((state) => (
+                                <option key={state} value={state}>
+                                    {state}
+                                </option>
+                            ))}
+                        </select>
                     </div>
 
                     <div className='space-y-1'>
@@ -136,7 +187,7 @@ const ProfileManagement = () => {
                                 value={zipCode}
                                 id='zipCode'
                                 name='zipCode'
-                                onChange={(e)=>setZipCode(e.currentTarget.value)}
+                                onChange={(e) => setZipCode(e.currentTarget.value)}
                                 required
                                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                             />
