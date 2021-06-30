ba35e7619ce8fb8ba928283345498bec975ee740
Michael McQuade <michael@giraffesyo.io>
2021-06-30 18:35:53 -0500

    Add logout page

---
+++ Software Design - COSC 6353/cosc6353-software-design/web/pages/logout.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/dist/client/router'
+import { useEffect } from 'react'
+import Layout from '../components/Layout'
+
+const LogoutPage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    // TODO: Log the user out
+    // then redirect
+    setTimeout(() => router.push('/'), 500)
+  }, [])
+  return (
+    <div className='bg-fuel text-center text-4xl font-medium h-screen flex justify-center items-center'>
+      <h1 className='text-white'>Logging out...</h1>
+    </div>
+  )
+}
+
+export default LogoutPage
