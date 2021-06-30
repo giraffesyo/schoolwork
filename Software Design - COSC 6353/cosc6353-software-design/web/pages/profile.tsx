81e41751bae20ad3a19cc3a21339298de5a146b8
Gal Egozi <8800898+galegozi@users.noreply.github.com>
2021-06-30 18:06:48 -0500

    Merge pull request #5 from galegozi/dev/gal
    Dev/gal

--- Software Design - COSC 6353/cosc6353-software-design/web/pages/profile.tsx
+++ Software Design - COSC 6353/cosc6353-software-design/web/pages/profile.tsx
@@ -1,80 +1,88 @@
+//TODO: State proper selector.
 // import TestConnect from '../components/TestConnect'
 
 import { useState } from 'react'
 
 const ProfileManagement = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
     const [name, setName] = useState('')
     const [addr1, setAddr1] = useState('')
     const [addr2, setAddr2] = useState('')
     const [city, setCity] = useState('')
     const [state, setState] = useState('')
     const [zipCode, setZipCode] = useState('')
-    /*
-      - State (Drop Down, selection required) DB will store 2 character state code
-      - Zipcode (9 characters, at least 5 character code required)
-     */
 
     const profMan = () => {
-        // TODO: Send username and password to the backend for verifcation
+        // TODO: Send info to the backend for verifcation
         // TODO: Set UI to loading state
     }
 
-    const getStates = () => {
-        // TODO: Access states from backend.
-        return [
-            "TX",
-            "US",
-            "IL"
-        ];
-    }
+    const states = {
+        AL: "Alabama",
+        AK: "Alaska",
+        AS: "American Samoa",
+        AZ: "Arizona",
+        AR: "Arkansas",
+        CA: "California",
+        CO: "Colorado",
+        CT: "Connecticut",
+        DE: "Delaware",
+        DC: "District Of Columbia",
+        FM: "Federated States Of Micronesia",
+        FL: "Florida",
+        GA: "Georgia",
+        GU: "Guam",
+        HI: "Hawaii",
+        ID: "Idaho",
+        IL: "Illinois",
+        IN: "Indiana",
+        IA: "Iowa",
+        KS: "Kansas",
+        KY: "Kentucky",
+        LA: "Louisiana",
+        ME: "Maine",
+        MH: "Marshall Islands",
+        MD: "Maryland",
+        MA: "Massachusetts",
+        MI: "Michigan",
+        MN: "Minnesota",
+        MS: "Mississippi",
+        MO: "Missouri",
+        MT: "Montana",
+        NE: "Nebraska",
+        NV: "Nevada",
+        NH: "New Hampshire",
+        NJ: "New Jersey",
+        NM: "New Mexico",
+        NY: "New York",
+        NC: "North Carolina",
+        ND: "North Dakota",
+        MP: "Northern Mariana Islands",
+        OH: "Ohio",
+        OK: "Oklahoma",
+        OR: "Oregon",
+        PW: "Palau",
+        PA: "Pennsylvania",
+        PR: "Puerto Rico",
+        RI: "Rhode Island",
+        SC: "South Carolina",
+        SD: "South Dakota",
+        TN: "Tennessee",
+        TX: "Texas",
+        UT: "Utah",
+        VT: "Vermont",
+        VI: "Virgin Islands",
+        VA: "Virginia",
+        WA: "Washington",
+        WV: "West Virginia",
+        WI: "Wisconsin",
+        WY: "Wyoming"
+    };
 
     return (
         <div className='mt-8'>
             Profile Management
             <div className='mt-6'>
                 <form className='space-y-6'>
-                    <div>
-                        <label
-                            htmlFor='username'
-                            className='block text-sm font-medium text-gray-700'
-                        >
-                            Username
-                        </label>
-                        <div className='mt-1'>
-                            <input
-                                value={username}
-                                id='username'
-                                name='username'
-                                onChange={(e) => setUsername(e.currentTarget.value)}
-                                required
-                                className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                            />
-                        </div>
-                    </div>
-
-                    <div className='space-y-1'>
-                        <label
-                            htmlFor='password'
-                            className='block text-sm font-medium text-gray-700'
-                        >
-                            Password
-                        </label>
-                        <div className='mt-1'>
-                            <input
-                                value={password}
-                                onChange={(e) => setPassword(e.currentTarget.value)}
-                                id='password'
-                                name='password'
-                                type='password'
-                                autoComplete='current-password'
-                                required
-                                className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                            />
-                        </div>
-                    </div>
-
                     <div className='space-y-1'>
                         <label
                             htmlFor='name'
@@ -87,7 +95,7 @@ const ProfileManagement = () => {
                                 value={name}
                                 id='name'
                                 name='name'
-                                onChange={(e)=>setName(e.currentTarget.value)}
+                                onChange={(e) => setName(e.currentTarget.value)}
                                 required
                                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                             />
@@ -106,7 +114,7 @@ const ProfileManagement = () => {
                                 value={addr1}
                                 id='addr1'
                                 name='addr1'
-                                onChange={(e)=>setAddr1(e.currentTarget.value)}
+                                onChange={(e) => setAddr1(e.currentTarget.value)}
                                 required
                                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                             />
@@ -125,7 +133,7 @@ const ProfileManagement = () => {
                                 value={addr2}
                                 id='addr2'
                                 name='addr2'
-                                onChange={(e)=>setAddr2(e.currentTarget.value)}
+                                onChange={(e) => setAddr2(e.currentTarget.value)}
                                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                             />
                         </div>
@@ -143,7 +151,43 @@ const ProfileManagement = () => {
                                 value={city}
                                 id='city'
                                 name='city'
-                                onChange={(e)=>setCity(e.currentTarget.value)}
+                                onChange={(e) => setCity(e.currentTarget.value)}
+                                required
+                                className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label
+                            htmlFor='state'
+                            className='block text-sm font-medium text-gray-700'
+                        >
+                            State
+                        </label>
+                        <select onChange={(e) => setState(e.currentTarget.value)} required>
+                            <option value=""></option>
+                            {Object.keys(states).map((state) => (
+                                <option key={state} value={state}>
+                                    {state}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className='space-y-1'>
+                        <label
+                            htmlFor='zipCode'
+                            className='block text-sm font-medium text-gray-700'
+                        >
+                            Zip Code
+                        </label>
+                        <div className='mt-1'>
+                            <input
+                                value={zipCode}
+                                id='zipCode'
+                                name='zipCode'
+                                onChange={(e) => setZipCode(e.currentTarget.value)}
                                 required
                                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                             />
