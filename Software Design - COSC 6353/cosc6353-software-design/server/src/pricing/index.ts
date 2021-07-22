a7d9a1de3e3737abf8616b657122f39fa835b8b9
Michael McQuade <michael@giraffesyo.io>
2021-07-21 19:38:06 -0500

    Pull out pricing module from route

---
+++ Software Design - COSC 6353/cosc6353-software-design/server/src/pricing/index.ts
@@ -0,0 +1,32 @@
+export interface calculatePriceParams {
+  state: string;
+  hasPrevQuotes: boolean;
+  gallons: number;
+}
+const calculatePrice = ({
+  state,
+  hasPrevQuotes,
+  gallons,
+}: calculatePriceParams) => {
+  const currPrice = 1.5;
+
+  const locFactor = state != 'TX' ? 0.04 : 0.02;
+
+  const histFactor = hasPrevQuotes ? 0.01 : 0.0;
+
+  const bigFactor = gallons > 1000 ? 0.02 : 0.03;
+
+  const profitFactor = 0.1;
+
+  const margin =
+    (locFactor - histFactor + bigFactor + profitFactor) * currPrice;
+  const ppg = margin + currPrice;
+
+  const totalPrice = ppg * gallons;
+
+  const storedPrice = Math.ceil(totalPrice * 100);
+
+  return { storedPrice, ppg, currPrice };
+};
+
+export default calculatePrice;
--- Software Design - COSC 6353/cosc6353-software-design/server/src/users/quote.ts
+++ Software Design - COSC 6353/cosc6353-software-design/server/src/users/quote.ts
@@ -1,9 +1,11 @@
-import DBClient from '../database/client'
-import { Router } from 'express'
-import passport, { use } from 'passport'
-const prisma = DBClient.getInstance().prisma
+import DBClient from '../database/client';
+import { Router } from 'express';
+import passport from 'passport';
+import calculatePrice from '../pricing';
 
-const router = Router()
+const prisma = DBClient.getInstance().prisma;
+
+const router = Router();
 
 router.post(
   '/quote',
@@ -20,7 +22,7 @@ router.post(
         error: true,
         message:
           'A username and a body with gallons and delivery date are required.',
-      })
+      });
     }
     if (
       !req.body.gallon ||
@@ -30,63 +32,54 @@ router.post(
       return res.status(400).send({
         error: true,
         message: 'Gallons must be a positive integer.',
-      })
+      });
     }
 
     if (new Date(req.body.deliveryDate).toString() === 'Invalid Date') {
       return res.status(400).send({
         error: true,
         message: 'Delivery date is not a valid date.',
-      })
+      });
     }
 
-    const { id, username } = req.user as { username: string; id: number }
+    const { id, username } = req.user as { username: string; id: number };
 
-    const { gallon, deliveryDate } = req.body
-
-    const currPrice = 1.5
+    const { gallon, deliveryDate } = req.body;
 
     try {
       var userinfo = await prisma.clientinformation.findFirst({
         where: { user_id: id },
-      })
+      });
       if (!userinfo) {
         return res.status(400).json({
           error: true,
           message:
             'Information must be added to the profile before requesting quotes.',
-        })
+        });
       }
 
       try {
-        var prevQuotes = await prisma.fuelquote.findFirst({
+        const prevQuotes = await prisma.fuelquote.findFirst({
           where: { user_id: id },
-        })
+        });
 
-        //todo: compute location factor
-        var locFactor = userinfo.state != 'TX' ? 0.04 : 0.02
+        const hasPrevQuotes: boolean = !!prevQuotes;
+        const state: string = userinfo.state;
+        const gallons: number = req.body.gallon;
 
-        var histFactor = prevQuotes ? 0.01 : 0.0
-
-        var bigFactor = gallon > 1000 ? 0.02 : 0.03
-
-        const profitFactor = 0.1
-
-        var margin =
-          (locFactor - histFactor + bigFactor + profitFactor) * currPrice
-        var ppg = margin + currPrice
-
-        var totalPrice = ppg * gallon
-
-        var storedPrice = Math.ceil(totalPrice * 100)
+        const price = calculatePrice({
+          hasPrevQuotes,
+          state,
+          gallons: gallons,
+        });
 
         try {
           const quote = await prisma.fuelquote.create({
             data: {
               usercredentials: { connect: { id } },
               gallons: Number(gallon),
-              price_per_gallon: Math.ceil(ppg * 100),
-              total_price: storedPrice,
+              price_per_gallon: Math.ceil(price.ppg * 100),
+              total_price: price.storedPrice,
               delivery_date: new Date(deliveryDate),
               delivery_address: userinfo.address,
               delivery_city: userinfo.city,
@@ -94,14 +87,14 @@ router.post(
               delivery_zip: userinfo.zip,
               delivery_address2: userinfo.address2,
             },
-          })
-          return res.status(200).json({ error: false })
+          });
+          return res.status(200).json({ error: false });
         } catch (err) {
-          console.error(err)
+          console.error(err);
           return res.status(500).json({
             error: true,
             message: 'Unable to create',
-          })
+          });
         } finally {
         }
       } finally {
@@ -110,16 +103,16 @@ router.post(
       return res.status(500).json({
         error: true,
         message: 'An error occurred while fetching the user',
-      })
+      });
     }
   }
-)
+);
 
 router.get(
   '/quoteinfo',
   passport.authenticate('jwt', { session: false }),
   async (req, res) => {
-    const { id, username } = req.user as { username: string; id: number }
+    const { id, username } = req.user as { username: string; id: number };
 
     try {
       const quotes = await prisma.fuelquote.findMany({
@@ -132,18 +125,18 @@ router.get(
           total_price: true,
         },
         where: { user_id: id },
-      })
+      });
 
       return res.status(200).json({
         quotes,
-      })
+      });
     } catch (e) {
       return res.status(500).json({
         error: true,
         message: 'An error occurred while fetching the quotes',
-      })
+      });
     }
   }
-)
+);
 
-export default router
+export default router;
