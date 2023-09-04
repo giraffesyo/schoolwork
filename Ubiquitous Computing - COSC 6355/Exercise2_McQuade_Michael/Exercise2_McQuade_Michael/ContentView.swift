d999b9542911fca3f390b77f646c13e1c2422959
Michael McQuade <michael@giraffesyo.io>
2023-09-04 18:19:23 -0500

    buttons full width

--- Ubiquitous Computing - COSC 6355/Exercise2_McQuade_Michael/Exercise2_McQuade_Michael/ContentView.swift
+++ Ubiquitous Computing - COSC 6355/Exercise2_McQuade_Michael/Exercise2_McQuade_Michael/ContentView.swift
@@ -49,21 +49,25 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            // each button should be full width
             Button(action: randomCard, label: {
-                Text("Random Card").padding().fontWeight(.bold).foregroundColor(.white)
+                Text("Random Card").padding().fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity)
             }) // #eec292 hex color is tan
+            
             .background(Color(red: 238/255, green: 194/255, blue: 146/255))
+            
+            
             Button(action: nextCard, label: {
-                Text("Next Card").padding().fontWeight(.bold).foregroundColor(.white)
+                Text("Next Card").padding().fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity)
             }) // #f2671c hex color is orange
             .background(Color(red: 242/255, green: 103/255, blue: 28/255))
             
             Button(action: {}, label: {
-                Text("Card Selector").padding().fontWeight(.bold).foregroundColor(.white)
-            }) // #8e4235 hex color is brown
+                Text("Card Selector").padding().fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity)
+            }) // #8e4235 hex color is brown 
             .background(Color(red: 142/255, green: 66/255, blue: 53/255))
         }
-        .padding()
+
     }
 }
 
