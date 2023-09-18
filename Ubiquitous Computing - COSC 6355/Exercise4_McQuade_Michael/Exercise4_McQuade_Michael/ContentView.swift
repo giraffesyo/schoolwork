231f4c3de54bce98ffeb36d7ef3fd39c9de8ee13
Michael McQuade <michael@giraffesyo.io>
2023-09-17 20:00:08 -0500

    Initial Commit

---
+++ Ubiquitous Computing - COSC 6355/Exercise4_McQuade_Michael/Exercise4_McQuade_Michael/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Exercise4_McQuade_Michael
+//
+//  Created by Michael McQuade on 9/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ubiquitous Computing - COSC 6355/Exercise4_McQuade_Michael/Exercise4_McQuade_Michael/Exercise4_McQuade_MichaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise4_McQuade_MichaelApp.swift
+//  Exercise4_McQuade_Michael
+//
+//  Created by Michael McQuade on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Exercise4_McQuade_MichaelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
