68547ed95579781bf6fe02da4d339b4106b3d95a
Michael McQuade <michael@giraffesyo.io>
2023-10-21 15:05:29 -0500

    initial tab bar

--- Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/ContentView.swift
+++ Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/ContentView.swift
@@ -1,26 +1,53 @@
-//
-//  ContentView.swift
-//  semester_project_McQuade_Michael
-//
-//  Created by Michael McQuade on 10/21/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    ZStack {
+
+      TabView {
+        WorkoutView()
+          .tabItem {
+            Image(systemName: "house")
+            Text("Home")
+          }
+        WorkoutView()
+          .tabItem {
+            Image(systemName: "list.bullet")
+            Text("Workouts")
+
+          }
+        WorkoutView()
+          .tabItem {
+            Image(systemName: "person")
+            Text("Profile")
+          }
+      }.toolbarBackground(Color.accentColor).preferredColorScheme(.dark)
     }
+  }
+}
+
+struct WorkoutView: View {
+  var body: some View {
+    ZStack {
+      Color(.black)
+        .edgesIgnoringSafeArea(.all)
+
+      VStack {
+        Text("Workouts")
+          .font(.title)
+          .fontWeight(.bold)
+          .foregroundColor(.accentColor)
+        Image(systemName: "globe")
+          .imageScale(.large)
+          .foregroundColor(.accentColor)
+        Text("Hello, world!")
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
