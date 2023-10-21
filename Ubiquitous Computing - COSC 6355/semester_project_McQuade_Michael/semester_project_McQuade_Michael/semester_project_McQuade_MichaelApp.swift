4e5eb26f4d480002136c6dfb721abab517840f8c
Michael McQuade <michael@giraffesyo.io>
2023-10-21 13:03:48 -0500

    Initial Commit

---
+++ Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/semester_project_McQuade_MichaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  semester_project_McQuade_MichaelApp.swift
+//  semester_project_McQuade_Michael
+//
+//  Created by Michael McQuade on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct semester_project_McQuade_MichaelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
