13641b6585660e0c0c7170d922881f0aee1a1804
Michael McQuade <michael@giraffesyo.io>
2023-10-21 17:26:06 -0500

    make time white

--- Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/Views/WorkoutTimerView.swift
+++ Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/Views/WorkoutTimerView.swift
@@ -4,12 +4,11 @@ struct WorkoutTimerView: View {
   @Binding var currentWorkout: Workout
   var body: some View {
 
+    Text(currentWorkout.startedAt, style: .timer)
+      .font(.system(size: 60))
+      .fontWeight(.bold)
+      .foregroundColor(.white)
+      .padding()
+  }
 
-      Text(currentWorkout.startedAt, style: .timer)
-        .font(.system(size: 60))
-        .fontWeight(.bold)
-        .foregroundColor(.accentColor)
-        .padding()
-    }
-  
 }
