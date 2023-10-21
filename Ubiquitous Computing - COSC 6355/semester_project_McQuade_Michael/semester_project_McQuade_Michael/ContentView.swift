aad8b3c5ece38fa57b068264c367bd73f9adee4f
Michael McQuade <michael@giraffesyo.io>
2023-10-21 15:41:51 -0500

    start workout

--- Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/ContentView.swift
+++ Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/ContentView.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var activeWorkout = false
+  func startWorkout() {
+    activeWorkout = true
+  }
+
   var body: some View {
     ZStack {
       Color(.black)
         .edgesIgnoringSafeArea(.all)
       TabView {
-        WorkoutView()
+        (activeWorkout
+          ? AnyView(ActiveWorkoutView())
+         : AnyView(StartWorkoutView(startWorkout: startWorkout)))
           .tabItem {
             Text("Workout")
             Image(systemName: "figure.walk")
@@ -44,6 +51,59 @@ struct WorkoutView: View {
       Button(action: {
         print("Button pressed")
       }) {
+        Text("Start Workout")
+          .font(.title)
+          .fontWeight(.bold)
+          .foregroundColor(.accentColor)
+          .padding()
+          .background(Color(.black))
+          .cornerRadius(40)
+          .padding(10)
+          .overlay(
+            RoundedRectangle(cornerRadius: 40)
+              .stroke(Color.accentColor, lineWidth: 5)
+          )
+      }.padding()
+      Spacer()
+    }
+  }
+}
+
+struct ActiveWorkoutView: View {
+  var body: some View {
+
+    VStack {
+      Text("Workout")
+        .font(.title)
+        .fontWeight(.bold)
+        .foregroundColor(.accentColor)
+        .frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal)
+      Spacer()
+
+      Spacer()
+    }
+  }
+}
+
+struct StartWorkoutView: View {
+    var startWorkout: () -> Void
+    var body: some View {
+      
+    VStack {
+      Text("Workout")
+        .font(.title)
+        .fontWeight(.bold)
+        .foregroundColor(.accentColor)
+        .frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal)
+      Spacer()
+      Image(systemName: "figure.walk")
+        .resizable()
+        .frame(width: 100, height: 150)
+        .foregroundColor(.accentColor)
+
+        Button(action: {
+            self.startWorkout()
+        }) {
         Text("Start Workout")
           .font(.title)
           .fontWeight(.bold)
