038558de29f7f4f999c0712188797ee6872546f9
Michael McQuade <michael@giraffesyo.io>
2023-10-21 15:32:04 -0500

    move color into contenview

--- Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/ContentView.swift
+++ Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/ContentView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     ZStack {
+      Color(.black)
+        .edgesIgnoringSafeArea(.all)
       TabView {
         WorkoutView()
           .tabItem {
@@ -26,41 +28,36 @@ struct ContentView: View {
 
 struct WorkoutView: View {
   var body: some View {
-    ZStack {
-      Color(.black)
-        .edgesIgnoringSafeArea(.all)
 
-      VStack {
-        Text("Workouts")
+    VStack {
+      Text("Workout")
+        .font(.title)
+        .fontWeight(.bold)
+        .foregroundColor(.accentColor)
+        .frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal)
+      Spacer()
+      Image(systemName: "figure.walk")
+        .resizable()
+        .frame(width: 100, height: 150)
+        .foregroundColor(.accentColor)
+
+      Button(action: {
+        print("Button pressed")
+      }) {
+        Text("Start Workout")
           .font(.title)
           .fontWeight(.bold)
           .foregroundColor(.accentColor)
-          // align left
-          .frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal)
-        Spacer()
-        Image(systemName: "figure.walk")
-          .resizable()
-          .frame(width: 100, height: 150)
-          .foregroundColor(.accentColor)
-
-        Button(action: {
-          print("Button pressed")
-        }) {
-          Text("Start Workout")
-            .font(.title)
-            .fontWeight(.bold)
-            .foregroundColor(.accentColor)
-            .padding()
-            .background(Color(.black))
-            .cornerRadius(40)
-            .padding(10)
-            .overlay(
-              RoundedRectangle(cornerRadius: 40)
-                .stroke(Color.accentColor, lineWidth: 5)
-            )
-        }.padding()
-        Spacer()
-      }
+          .padding()
+          .background(Color(.black))
+          .cornerRadius(40)
+          .padding(10)
+          .overlay(
+            RoundedRectangle(cornerRadius: 40)
+              .stroke(Color.accentColor, lineWidth: 5)
+          )
+      }.padding()
+      Spacer()
     }
   }
 }
