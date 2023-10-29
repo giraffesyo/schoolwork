4ae890651b36c2424abe1a4cf2525e8c161f1131
Michael McQuade <michael@giraffesyo.io>
2023-10-29 15:39:44 -0500

    add dots for days

--- Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/Views/GoalsView.swift
+++ Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/Views/GoalsView.swift
@@ -1,5 +1,24 @@
 import SwiftUI
 
+struct Days {
+  static let Monday: String = "Mo"
+  static let Tuesday: String = "Tu"
+  static let Wednesday: String = "We"
+  static let Thursday: String = "Th"
+  static let Friday: String = "Fr"
+  static let Saturday: String = "Sa"
+  static let Sunday: String = "Su"
+  static let all: [String] = [
+    Days.Monday,
+    Days.Tuesday,
+    Days.Wednesday,
+    Days.Thursday,
+    Days.Friday,
+    Days.Saturday,
+    Days.Sunday,
+  ]
+}
+
 struct GoalsView: View {
   var body: some View {
 
@@ -11,7 +30,25 @@ struct GoalsView: View {
           .foregroundColor(.accentColor)
         Spacer()
       }
+      HStack {
+        ForEach(Days.all, id: \.self) { day in
+          DayView(day: day, exercised: false)
+        }
+      }
       Spacer()
+    }
+  }
+}
+
+struct DayView: View {
+  private var day = ""
+  private var exercised = false
+  var body: some View {
+    VStack {
+      Circle()
+        .frame(width: 10, height: 10)
+        .foregroundColor(exercised ? .accentColor : .black)
+      Text(day).foregroundColor(exercised ? .accentColor : .gray)
     }
   }
 }
