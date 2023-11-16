9e8a0d7711902cb3933d866f261b4e25bd035d19
Michael McQuade <michael@giraffesyo.io>
2023-11-16 15:11:38 -0700

    Initial Commit

---
+++ Ubiquitous Computing - COSC 6355/Exam3_McQuade_Michael/Exam3_McQuade_Michael Watch App/Exam3_McQuade_MichaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exam3_McQuade_MichaelApp.swift
+//  Exam3_McQuade_Michael Watch App
+//
+//  Created by Michael McQuade on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Exam3_McQuade_Michael_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ubiquitous Computing - COSC 6355/Exam3_McQuade_Michael/Exam3_McQuade_Michael/Exam3_McQuade_MichaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exam3_McQuade_MichaelApp.swift
+//  Exam3_McQuade_Michael
+//
+//  Created by Michael McQuade on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Exam3_McQuade_MichaelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
