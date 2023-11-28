5dc6938d4e3a8b43be0eea0856639a64b4f1fff2
Michael McQuade <michael@giraffesyo.io>
2023-11-27 23:34:55 -0600

    fix: color of text

--- Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/Views/ActiveWorkoutView.swift
+++ Ubiquitous Computing - COSC 6355/semester_project_McQuade_Michael/semester_project_McQuade_Michael/Views/ActiveWorkoutView.swift
@@ -140,7 +140,7 @@ struct ExerciseListView: View {
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                       keyboardFocused = true
                     }
-                  }.foregroundColor(.black)
+                  }.foregroundColor(.white)
                   Button("OK") {
                     showWeightPicker = false
                   }
